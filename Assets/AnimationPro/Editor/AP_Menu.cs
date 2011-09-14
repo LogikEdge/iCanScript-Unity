aeee8e80f9d6c4014b5415414e86053e0c6a55cd
Reinual <michel.launier@videotron.ca>
2011-09-14 16:35:23 -0400

    MENU: 1) Add menu items to create independent modules and state charts. 2) Rename in menu "Add Graph" to "Create Behaviour".
    Signed-off-by: Reinual <michel.launier@videotron.ca>

--- Assets/AnimationPro/Editor/AP_Menu.cs
+++ Assets/AnimationPro/Editor/AP_Menu.cs
@@ -5,17 +5,55 @@ using System.Collections;
 public class AP_Menu {
 
     // ======================================================================
-	// Add State to selected game object.
-	[MenuItem("AnimationPro/Add Graph")]
-	public static void AddGapGraph() {
-		// Add State Chart component.
+	// Create a behavior to selected game object.
+	[MenuItem("WarpDrive/Create Behaviour")]
+	public static void CreateBehaviour() {
+		// Create State Chart component.
 		AP_Graph gapGraph = Selection.activeGameObject.GetComponent<AP_Graph>();
 		if(gapGraph == null) {
 			Selection.activeGameObject.AddComponent<AP_Graph>();
 		}
 	}
-	[MenuItem("AnimationPro/Add Graph", true)]
-	public static bool ValidateAddGapGraph() {
+	[MenuItem("WarpDrive/Create Behaviour", true)]
+	public static bool ValidateCreateBehaviour() {
+		if(Selection.activeTransform != null) {
+			AP_Graph gapGraph = Selection.activeGameObject.GetComponent<AP_Graph>();
+			return gapGraph == null;
+		}
+		return false;
+	}
+
+    // ======================================================================
+	// Create a library module to selected game object.
+	[MenuItem("WarpDrive/Create Module Library")]
+	public static void CreateModuleLibrary() {
+		// Create State Chart component.
+		AP_Graph gapGraph = Selection.activeGameObject.GetComponent<AP_Graph>();
+		if(gapGraph == null) {
+			Selection.activeGameObject.AddComponent<AP_Graph>();
+		}
+	}
+	[MenuItem("WarpDrive/Create Module Library", true)]
+	public static bool ValidateCreateModuleLibrary() {
+		if(Selection.activeTransform != null) {
+			AP_Graph gapGraph = Selection.activeGameObject.GetComponent<AP_Graph>();
+			return gapGraph == null;
+		}
+		return false;
+	}
+
+    // ======================================================================
+	// Create a library module to selected game object.
+	[MenuItem("WarpDrive/Create State Chart Library")]
+	public static void CreateStateChartLibrary() {
+		// Create State Chart component.
+		AP_Graph gapGraph = Selection.activeGameObject.GetComponent<AP_Graph>();
+		if(gapGraph == null) {
+			Selection.activeGameObject.AddComponent<AP_Graph>();
+		}
+	}
+	[MenuItem("WarpDrive/Create State Chart Library", true)]
+	public static bool ValidateCreateStateChartLibrary() {
 		if(Selection.activeTransform != null) {
 			AP_Graph gapGraph = Selection.activeGameObject.GetComponent<AP_Graph>();
 			return gapGraph == null;
@@ -25,14 +63,10 @@ public class AP_Menu {
 
     // ======================================================================
 	// AnimationPro Window.
-	[MenuItem("Window/AnimationPro Editor")]
-	public static void ShowAnimationProEditor() {
+	[MenuItem("Window/WarpDrive Editor")]
+	public static void ShowWarpDriveEditor() {
         AP_Editor editor= EditorWindow.GetWindow<AP_Editor>();
         editor.hideFlags= HideFlags.DontSave;
-	}
-	[MenuItem("Window/AnimationPro Editor", true)]
-	public static bool ValidateShowAnimationProEditor() {
-        return true;
 	}
 
 }
