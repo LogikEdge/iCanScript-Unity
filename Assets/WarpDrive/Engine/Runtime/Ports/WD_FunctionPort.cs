ce6b111eaf53b1ed81b14abf740f8524310189ef
Reinual <michel.launier@videotron.ca>
2011-09-28 14:37:21 -0400

    Remove code relocated to Editor.
    Signed-off-by: Reinual <michel.launier@videotron.ca>

--- Assets/WarpDrive/Engine/Runtime/Ports/WD_FunctionPort.cs
+++ Assets/WarpDrive/Engine/Runtime/Ports/WD_FunctionPort.cs
@@ -7,7 +7,7 @@ public class WD_FunctionPort : WD_Port {
     // ======================================================================
     // Properties
     // ----------------------------------------------------------------------
-	[SerializeField] WD_FunctionPort    mySource= null;
+	public WD_FunctionPort    Source= null;
     
 
     // ======================================================================
@@ -57,7 +57,7 @@ public class WD_FunctionPort : WD_Port {
         return sourceAction.IsCurrent();        
     }
     public override WD_Port GetConnectedPort() {
-        return mySource;
+        return Source;
     }
 
     // ======================================================================
@@ -81,83 +81,9 @@ public class WD_FunctionPort : WD_Port {
     }
     
     // ----------------------------------------------------------------------
-    public WD_FunctionPort Source {
-        get { return mySource; }
-        set {
-            // Nothing to do if we are already connected to source.
-            if(value == mySource) return;
-
-            // Compute new type.
-            WD_FunctionPort thePrevSource= mySource;
-            mySource= value;
-            List<WD_FunctionPort> connectedPorts= GetConnectedPorts();
-            WD_FunctionPort bestUpConversionPort= GetBestUpConversionPort(connectedPorts);
-
-            // Reject request if type mismatch.
-            if(bestUpConversionPort == null) {
-                mySource= thePrevSource;
-                return;
-            }
-            ReconfigurePortChain();
-            
-            // Recompute type for disconnected source.
-            if(thePrevSource != null) {
-                connectedPorts= thePrevSource.GetConnectedPorts();
-                thePrevSource.ReconfigurePortChain();
-            }            
-        }
-    }
-    // ----------------------------------------------------------------------
-    public bool TrySetSource(WD_FunctionPort theNewSource) {
-        Source= theNewSource;
-        return Source == theNewSource;
-    }
-    
-    // ----------------------------------------------------------------------
-    public void ReconfigurePortChain() {
-        // Cleanup virtual ports that are not the last port in chain.
-        List<WD_FunctionPort> connectedPorts= GetConnectedPorts();
-        if(connectedPorts.Count == 1) {
-            if(connectedPorts[0].IsVirtual) {
-                connectedPorts[0].AsVirtual.ConcretePort= null;
-            }
-            return;
-        }
-        foreach(var port in connectedPorts) {
-            if(port.IsVirtual && port.mySource != null) {
-                WD_VirtualDataPort vPort= port.AsVirtual;
-                vPort.ConcretePort= null;
-            }
-        }
-        // The chain is properly configured if the last port is concrete.
-        WD_FunctionPort lastPort= GetLastSourcePort();
-        if(!lastPort.IsVirtual) return;
-        
-        // Assign the virtual port to the best up conversion port if it is not initialized.
-        WD_VirtualDataPort virtualPort= lastPort.AsVirtual;
-        WD_FunctionPort bestUpConversionPort= GetBestUpConversionPort(connectedPorts);
-        if(virtualPort.ConcretePort == null || bestUpConversionPort == null) {
-            virtualPort.ConcretePort= bestUpConversionPort;
-            return;
-        }
-        // Verify that the virtual port is of the proper type.
-        if(virtualPort.ConcretePort.ValueType != bestUpConversionPort.ValueType) {
-            bestUpConversionPort.UpdateValue();
-            virtualPort.ConcretePort= bestUpConversionPort;
-            return;
-        }
-        // Verify that the virtual port actually points to a connected port.
-        foreach(var port in connectedPorts) {
-            if(port == virtualPort.ConcretePort) return;
-        }
-        bestUpConversionPort.UpdateValue();
-        virtualPort.ConcretePort= bestUpConversionPort;
-    }
-    
-    // ----------------------------------------------------------------------
     public WD_FunctionPort GetLastSourcePort() {
         WD_FunctionPort port= this;
-        for(; port.mySource != null; port= port.mySource);
+        for(; port.Source != null; port= port.Source);
         return port;
     }
 
@@ -293,76 +219,6 @@ public class WD_FunctionPort : WD_Port {
         Debug.LogWarning("Unable to convert port types from "+sourceFieldType.Name+" to "+destFieldType.Name);
     }
 
-    // ----------------------------------------------------------------------
-    // Returns a list of all ports connected together.
-    public List<WD_FunctionPort> GetConnectedPorts() {
-        // Filled the result with the connected ports.
-        List<WD_FunctionPort> theConnectedPorts= new List<WD_FunctionPort>();
-        for(WD_FunctionPort linkedPort= this; linkedPort != null; linkedPort= linkedPort.Source) {
-            theConnectedPorts.Add(linkedPort);            
-        }
-        DoGetConnectedPorts(theConnectedPorts);
-        return theConnectedPorts;
-    }
-    
-    // ----------------------------------------------------------------------
-    // Core function to add ports to the connection port list.
-    public virtual void DoGetConnectedPorts(List<WD_FunctionPort> theConnectedPorts) {
-        Top.ForEachRecursive<WD_FunctionPort>(
-            (port)=> {
-                WD_FunctionPort source= port.Source;
-                if(source != null) {
-                    foreach(var foundPort in theConnectedPorts) {
-                        if(foundPort == source) {
-                            if(AddPortUniqu(theConnectedPorts, port)) {
-                                DoGetConnectedPorts(theConnectedPorts);
-                                return;
-                            }
-                        }
-                    }
-                }
-            }
-        );
-    }
-
-    // ----------------------------------------------------------------------
-    // Adds a port into the port list if it does not already exists.
-    protected static bool AddPortUniqu(List<WD_FunctionPort> thePortList, WD_FunctionPort theNewPort) {
-        foreach(WD_FunctionPort thePortIter in thePortList) {
-            if(thePortIter == theNewPort) return false;
-        }
-        thePortList.Add(theNewPort);
-        return true;
-    }
-    
-    // ----------------------------------------------------------------------
-    public static WD_FunctionPort GetBestUpConversionPort(List<WD_FunctionPort> portList) {
-        if(portList.Count < 1) return null;
-        WD_FunctionPort bestPort= null;
-        int i= 0;
-        for(; i < portList.Count; ++i) {
-            if(!portList[i].IsVirtual) {
-                bestPort= portList[i];
-                ++i;
-                break;
-            }
-        }
-        if(bestPort == null) return null;
-        System.Type bestType= bestPort.ElementType;
-        for(; i < portList.Count; ++i) {
-            if(!portList[i].IsVirtual) {
-                System.Type portType= portList[i].ElementType;
-                System.Type upConversionType= WD_TypeSystem.GetBestUpConversionType(bestType, portType);
-                if(upConversionType == null) return null;
-                if(upConversionType != bestType) {
-                    bestPort= portList[i];
-                    bestType= portType;
-                }                
-            }
-        }
-        return bestPort;
-    }
-    
 	// ----------------------------------------------------------------------
     // Removes this port from all connections.
     public void Disconnect() {
