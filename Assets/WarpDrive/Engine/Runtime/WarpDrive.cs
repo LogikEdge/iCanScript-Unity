4cebf98235365fd1b1fc4d9c21fe04f55f7b3f5c
Reinual <michel.launier@videotron.ca>
2011-10-05 14:20:02 -0400

    Creation of a global WarpDrive class to contain static methods used by multiple internal classes.
    Signed-off-by: Reinual <michel.launier@videotron.ca>

---
+++ Assets/WarpDrive/Engine/Runtime/WarpDrive.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+
+// ===========================================================================
+// Set of global utilities used by multiple WarpDrive classes.
+public class WarpDrive {
+    // ---------------------------------------------------------------------------
+
+}
