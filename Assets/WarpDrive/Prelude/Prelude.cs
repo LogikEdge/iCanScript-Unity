ec7dd36d5022c0d37edb083aa8f72284f0ec0d01
Michel Launier <michel.launier@videotron.ca>
2011-10-21 22:20:56 -0400

    Prelude code cleanup.
    Signed-off-by: Michel Launier <michel.launier@videotron.ca>

--- Assets/WarpDrive/Prelude/Prelude.cs
+++ Assets/WarpDrive/Prelude/Prelude.cs
@@ -8,27 +8,34 @@ using System.Collections.Generic;
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 public static partial class Prelude {
     // ----------------------------------------------------------------------
-    public static A id<A>(A a) { return a; }
-    
+    // id :: a->a
+    public static A id<A>(A a)                 { return a; }
+
     // ----------------------------------------------------------------------
-    public static R dot<A,B,R>(System.Func<B,R> f1, System.Func<A,B> f2, A a) {
+    // constant :: a->b->a
+    public static A constant<A,B>(A a, B b)    { return a; }
+    public static Func<B,A> constant<A,B>(A a) { return delegate(B b) { return constant(a,b); }; }
+
+    // ----------------------------------------------------------------------
+    // dot :: (b->c)->(a->b)->a->c
+    public static C dot<A,B,C>(Func<B,C> f1, Func<A,B> f2, A a) {
         return f1(f2(a));
     }
-    public static System.Func<A,R> dot<A,B,R>(System.Func<B,R> f1, System.Func<A,B> f2) {
+    public static Func<A,R> dot<A,B,R>(Func<B,R> f1, Func<A,B> f2) {
         return delegate(A a) { return dot(f1,f2,a); };
     }
-    public static System.Func<System.Func<A,B>,System.Func<A,R>> dot<A,B,R>(System.Func<B,R> f1) {
-        return delegate(System.Func<A,B> f2) { return dot(f1,f2); };
+    public static Func<Func<A,B>, Func<A,R>> dot<A,B,R>(Func<B,R> f1) {
+        return delegate(Func<A,B> f2) { return dot(f1,f2); };
     }
     
     // ----------------------------------------------------------------------
-    public static R flip<A,B,R>(System.Func<B,A,R> f, A a, B b) { return f(b,a); }
-    public static Func<B,R> flip<A,B,R>(Func<B,A,R> f, A a) {
-        return delegate(B b) { return flip(f,a,b); };
+    // flip :: (a->b->c)->b->a->c
+    public static C flip<A,B,C>(Func<A,B,C> f, B b, A a) { return f(a,b); }
+    public static Func<A,C> flip<A,B,C>(Func<A,B,C> f, B b) {
+        return delegate(A a) { return flip(f,b,a); };
     }
-    public static Func<A,Func<B,R>> flip<A,B,R>(Func<B,A,R> f) {
-        return delegate(A a) { return flip(f,a); };
+    public static Func<B,Func<A,C>> flip<A,B,C>(Func<A,B,C> f) {
+        return delegate(B b) { return flip(f,b); };
     }
-
 
 }
