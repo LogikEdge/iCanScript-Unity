a40e7a306cec1f8855bfa44031d15b2c0c4bb450
Reinual <michel.launier@videotron.ca>
2011-10-23 09:59:45 -0400

    relocate unity functions under a Unity folder.  The company name for these functions are also renamed to "Unity".
    Signed-off-by: Reinual <michel.launier@videotron.ca>

--- Assets/WarpDrive/Engine/Runtime/Function/Unity/Debug/WD_Debug.cs
+++ Assets/WarpDrive/Engine/Runtime/Function/Unity/Debug/WD_Debug.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-[WD_Class(Company="Infaunier", Package="Debug", Name="Debug")]
+[WD_Class(Company="Unity", Package="Debug", Name="Debug")]
 public class WD_Debug {
     [WD_Function] public static void Log(string message) { Debug.Log(message); }
     [WD_Function] public static void LogWarning(string message) { Debug.LogWarning(message); }
