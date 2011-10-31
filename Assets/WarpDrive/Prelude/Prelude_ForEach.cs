7ae8b05c997de9b3d58e57d4ef9dac70ab3ab588
Reinual <michel.launier@videotron.ca>
2011-10-31 11:14:54 -0400

    Support forEach with conditional clause.
    Signed-off-by: Reinual <michel.launier@videotron.ca>

--- Assets/WarpDrive/Prelude/Prelude_ForEach.cs
+++ Assets/WarpDrive/Prelude/Prelude_ForEach.cs
@@ -5,11 +5,20 @@ using System.Collections.Generic;
 
 public static partial class Prelude {
     // ----------------------------------------------------------------------
-    // forEach :: (f a->())->[a]->()
+    // forEach :: (a->())->[a]->()
+    // forEach :: (a->bool)->[a]->bool
     public static void forEach<A>(Action<A> action, A[] lst) {
         foreach(var obj in lst) action(obj);
     }
     public static void forEach<A>(Action<A> action, List<A> lst) {
         foreach(var obj in lst) action(obj);
+    }
+    public static bool forEach<A>(Func<A,bool> fnc, A[] lst) {
+        foreach(var obj in lst) if(fnc(obj)) return true;
+        return false;
+    }
+    public static bool forEach<A>(Func<A,bool> fnc, List<A> lst) {
+        foreach(var obj in lst) if(fnc(obj)) return true;
+        return false;
     }
 }
