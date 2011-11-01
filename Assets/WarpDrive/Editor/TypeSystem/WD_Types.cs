807655d10be2c60f8ed07d43b1508d3396d6a08c
Reinual <michel.launier@videotron.ca>
2011-11-01 18:07:32 -0400

    Allow connection between types regardless of reference or array construct.
    Signed-off-by: Reinual <michel.launier@videotron.ca>

--- Assets/WarpDrive/Editor/TypeSystem/WD_Types.cs
+++ Assets/WarpDrive/Editor/TypeSystem/WD_Types.cs
@@ -5,7 +5,7 @@ using System.Collections;
 public static class WD_Types {
 	// ----------------------------------------------------------------------
     public static bool CanBeConnectedWithoutConversion(Type outType, Type inType) {
-        return GetDataType(outType) == inType;
+        return GetDataType(outType) == GetDataType(inType);
     }
 	// ----------------------------------------------------------------------
     public static Type GetDataType(Type t) {
