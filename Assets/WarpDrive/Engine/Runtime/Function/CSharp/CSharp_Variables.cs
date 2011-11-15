9eb362579db615b2c9777f1acc2f409bd7ca4a97
Reinual <michel.launier@videotron.ca>
2011-11-15 12:21:15 -0500

    Function code reorganization.
    Signed-off-by: Reinual <michel.launier@videotron.ca>

---
+++ Assets/WarpDrive/Engine/Runtime/Function/CSharp/CSharp_Variables.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+
+[WD_Class(Company="CSharp", Package="Variables")]
+public class CSharp_Variables {
+    [WD_Function(Name="int")]       public static int       _int(int value)         { return value; }
+    [WD_Function(Name="float")]     public static float     _int(float value)       { return value; }
+    [WD_Function(Name="string")]    public static string    _string(string value)   { return value; }
+}
--- Assets/WarpDrive/Engine/Runtime/Function/Unity/Unity_Debug.cs
+++ Assets/WarpDrive/Engine/Runtime/Function/Unity/Unity_Debug.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 [WD_Class(Company="Unity", Package="Debug", Name="Debug")]
-public class WD_Debug {
+public class Unity_Debug {
     [WD_Function] public static string Log(string message)        { Debug.Log(message); return message; }
     [WD_Function] public static string LogWarning(string message) { Debug.LogWarning(message); return message; }
     [WD_Function] public static string LogError(string message)   { Debug.LogError(message); return message; }
