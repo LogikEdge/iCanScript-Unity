1642e21b81ad08004b1f8c0c03b99b71f7a2f409
Michel Launier <michel.launier@videotron.ca>
2011-11-16 14:06:59 -0500

    Remove references to company Microsoft.
    Signed-off-by: Michel Launier <michel.launier@videotron.ca>

--- Assets/WarpDrive/Engine/Runtime/Function/CSharp/CSharp.cs
+++ Assets/WarpDrive/Engine/Runtime/Function/CSharp/CSharp.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using System.Collections;
 
-[WD_Class(Company="Microsoft", Package="CSharp")]
+[WD_Class(Company="CSharp", Package="String")]
 public class CSharp {
-    [WD_Function]   public static string FloatToString(float v) { return v.ToString(); }
+    [WD_Function]   public static string ToString(int v)    { return v.ToString(); }
+    [WD_Function]   public static string ToString(float v)  { return v.ToString(); }
 }
