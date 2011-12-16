146f9811d094dfc0eb46d02144832b275b500e60
Reinual <michel.launier@videotron.ca>
2011-12-16 17:51:54 -0500

    Support Obfuscator when publishing libraries.
    Signed-off-by: Reinual <michel.launier@videotron.ca>

--- publish.sh
+++ publish.sh
@@ -1,7 +1,1 @@
-rm -r ../iCanScriptPublish/Assets/iCanScript
-mkdir ../iCanScriptPublish/Assets/iCanScript
-mkdir ../iCanScriptPublish/Assets/iCanScript/Editor
-mkdir ../iCanScriptPublish/Assets/iCanScript/Engine
-cp -r Assets/iCanScript/Editor/Resources ../iCanScriptPublish/Assets/iCanScript/Editor/Resources
-cp Library/ScriptAssemblies/Assembly-CSharp-Editor.dll ../iCanScriptPublish/Assets/iCanScript/Editor/iCanScriptEditor.dll
-cp Library/ScriptAssemblies/Assembly-CSharp.dll ../iCanScriptPublish/Assets/iCanScript/Engine/iCanScriptEngine.dll
+cd publish; ./publish.sh
---
+++ publish/publish.sh
@@ -0,0 +1,13 @@
+rm -r ../../iCanScriptPublish/Assets/iCanScript
+mkdir ../../iCanScriptPublish/Assets/iCanScript
+mkdir ../../iCanScriptPublish/Assets/iCanScript/Editor
+mkdir ../../iCanScriptPublish/Assets/iCanScript/Engine
+cp -r ../Assets/iCanScript/Editor/Resources ../../iCanScriptPublish/Assets/iCanScript/Editor/Resources
+
+cp /Applications/Unity/Unity.app/Contents/Frameworks/Managed/UnityEngine.dll Obfuscar_Input
+cp /Applications/Unity/Unity.app/Contents/Frameworks/Managed/UnityEditor.dll Obfuscar_Input
+cp ../Library/ScriptAssemblies/Assembly-CSharp-Editor.dll Obfuscar_Input
+cp ../Library/ScriptAssemblies/Assembly-CSharp.dll Obfuscar_Input
+mono Obfuscar.exe obfuscar.xml
+cp Obfuscar_Output/Assembly-CSharp-Editor.dll ../../iCanScriptPublish/Assets/iCanScript/Editor/iCanScriptEditor.dll
+cp Obfuscar_Output/Assembly-CSharp.dll ../../iCanScriptPublish/Assets/iCanScript/Engine/iCanScriptEngine.dll
