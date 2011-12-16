c1df3382f7975944c4dbf68704a59f0426021a36
Reinual <michel.launier@videotron.ca>
2011-12-16 12:58:26 -0500

    Change background theme for iCanScript web page.
    Signed-off-by: Reinual <michel.launier@videotron.ca>

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
-rm -r ../iCanScriptPublish/Assets/Nodeling
-mkdir ../iCanScriptPublish/Assets/Nodeling
-mkdir ../iCanScriptPublish/Assets/Nodeling/Editor
-mkdir ../iCanScriptPublish/Assets/Nodeling/Engine
-cp -r Assets/Nodeling/Editor/Resources ../iCanScriptPublish/Assets/Nodeling/Editor/Resources
-cp Library/ScriptAssemblies/Assembly-CSharp-Editor.dll ../iCanScriptPublish/Assets/Nodeling/Editor/NodelingEditor.dll
-cp Library/ScriptAssemblies/Assembly-CSharp.dll ../iCanScriptPublish/Assets/Nodeling/Engine/NodelingEngine.dll
+rm -r ../iCanScriptPublish/Assets/iCanScript
+mkdir ../iCanScriptPublish/Assets/iCanScript
+mkdir ../iCanScriptPublish/Assets/iCanScript/Editor
+mkdir ../iCanScriptPublish/Assets/iCanScript/Engine
+cp -r Assets/iCanScript/Editor/Resources ../iCanScriptPublish/Assets/iCanScript/Editor/Resources
+cp Library/ScriptAssemblies/Assembly-CSharp-Editor.dll ../iCanScriptPublish/Assets/iCanScript/Editor/iCanScriptEditor.dll
+cp Library/ScriptAssemblies/Assembly-CSharp.dll ../iCanScriptPublish/Assets/iCanScript/Engine/iCanScriptEngine.dll
