8bdc7f78a1661d5ff19ca1bfb025c36597531447
Michel Launier <michel.launier@videotron.ca>
2011-12-27 06:26:10 -0500

    Support encoding/decoding object of derived types.

--- Assets/iCanScript/Engine/Runtime/DataBase/iCS_Archive.cs
+++ Assets/iCanScript/Engine/Runtime/DataBase/iCS_Archive.cs
@@ -7,18 +7,26 @@ public class iCS_Archive {
     // ======================================================================
     //  string to/from object conversions
 	// ----------------------------------------------------------------------
-    public static string Encode(Type type) {
-        return type.AssemblyQualifiedName;
-    }
     public static string Encode(object obj) {
+		return Encode(obj, obj.GetType());
+	}
+    public static string Encode(object obj, Type expectedType) {
+		if(obj is Type) {
+			Type type= obj as Type;
+	        return "{"+type.AssemblyQualifiedName+"}\n";
+		}
+		// Put type override if type is not the expected type.
         Type objType= obj.GetType();
+		if(objType != expectedType) {
+			return "{"+Encode(objType, typeof(Type))+Encode(obj, objType)+"}\n";
+		}
         if(objType.IsEnum) {
             int enumValue= (int)obj;
-            return enumValue.ToString()+"("+obj.ToString()+")";
+            return "{"+enumValue.ToString()+"("+obj.ToString()+")}\n";
         }
         if(objType == typeof(string)) {
             string value= obj as string;
-            string encoded= "\"";
+            string encoded= "{\"";
             for(int i= 0; i < value.Length; ++i) {
                 switch(value[i]) {
                     case '\\': {
@@ -35,23 +43,23 @@ public class iCS_Archive {
                     }
                 }
             }
-            return encoded+"\"";
+            return encoded+"\"}\n";
         }
         if(objType == typeof(Vector2)) {
             Vector2 v= (Vector2)obj;
-            return "("+v.x+","+v.y+")";
+            return "{("+v.x+","+v.y+")}\n";
         }
         if(objType == typeof(Vector3)) {
             Vector3 v= (Vector3)obj;
-            return "("+v.x+","+v.y+","+v.z+")";
+            return "{("+v.x+","+v.y+","+v.z+")}\n";
         }
         if(objType == typeof(Vector4)) {
             Vector4 v= (Vector4)obj;
-            return "("+v.x+","+v.y+","+v.z+","+v.w+")";
+            return "{("+v.x+","+v.y+","+v.z+","+v.w+")}\n";
         }
         // Use converter for all remaining types.
         try {
-            return (string)Convert.ChangeType(obj, typeof(string));            
+            return "{"+(string)Convert.ChangeType(obj, typeof(string))+"}\n";            
         }
         catch(Exception) {
 //            Debug.LogWarning("Unable to encode object of type: "+objType.Name);
@@ -65,9 +73,23 @@ public class iCS_Archive {
     }
 	// ----------------------------------------------------------------------
     public static object Decode(string valueStr, Type type) {
+		return DecodeNoMarkers(RemoveMarkers(valueStr), type);
+    }
+	// ----------------------------------------------------------------------
+    public static T DecodeNoMarkers<T>(string valueStr) {
+        return (T)DecodeNoMarkers(valueStr, typeof(T));
+    }
+	// ----------------------------------------------------------------------
+    public static object DecodeNoMarkers(string valueStr, Type type) {
         if(type == typeof(Type)) {
             return Type.GetType(valueStr);
         }
+		// Check for type override.
+		if(valueStr[0] == '{') {
+			string typeStr= ExtractDecodeString(ref valueStr);
+			Type objType= Type.GetType(typeStr);			
+			return Decode(valueStr, objType);
+		}
         if(type == typeof(string)) {
             string value= "";
             int end= valueStr.IndexOf('"');
@@ -137,8 +159,52 @@ public class iCS_Archive {
     static bool DecodeWithSeperator<T>(ref string valueStr, char seperator, out T value) {
         int end= valueStr.IndexOf(seperator);
         if(end < 0) { Debug.LogWarning("Decode: Invalid "+typeof(T).Name+" format !!!"); value= default(T); return false; }
-        value= Decode<T>(valueStr.Substring(0, end));
+        value= DecodeNoMarkers<T>(valueStr.Substring(0, end));
         valueStr= valueStr.Substring(end+1, valueStr.Length-end-1);
         return true;
     }
+	// ----------------------------------------------------------------------
+	public static string ExtractDecodeStringWithMarkers(ref string encoded) {
+		int len= encoded.Length;
+		if(len < 3) {
+			Debug.LogError("Decode: formatting error: encoded string does not have the proper markers {...}");			
+			encoded= "";
+			return null;
+		}
+		int end= FindDecodeEnd(encoded);
+		string toDecode= encoded.Substring(0, end);
+		encoded= encoded.Substring(end);
+		return toDecode;
+	}
+	// ----------------------------------------------------------------------
+	public static string ExtractDecodeString(ref string encoded) {
+		return RemoveMarkers(ExtractDecodeStringWithMarkers(ref encoded));
+	}
+	// ----------------------------------------------------------------------
+	public static string RemoveMarkers(string toDecode) {
+		int len= toDecode.Length;
+		if(toDecode[0] != '{' || toDecode[len-2] != '}' || toDecode[len-1] != '\n') {
+			Debug.LogError("Decode: formatting error: encoded string does not have the proper markers {...}");			
+			Debug.Log("Decode string is: "+toDecode);
+			return null;
+		}
+		return toDecode.Substring(1, len-3);
+	}
+	// ----------------------------------------------------------------------
+	static int FindDecodeEnd(string encoded) {
+		int i;
+		for(i= 0; i < encoded.Length; ++i) {
+			if(encoded[i] == '\n') {
+				if(i == 0) {
+					++i;
+					break;
+				}
+				if(encoded[i-1] == '}') {
+					++i;
+					break;
+				}
+			}
+		}
+		return i;		
+	}
 }
