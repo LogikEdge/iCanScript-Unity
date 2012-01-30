f554f22f651d1d9614297f26ba146da099d0675c
Reinual <michel.launier@videotron.ca>
2012-01-30 16:56:04 -0500

    Bug Fix: Libraries created from a drag operation now uses the initial layout of the source nodes.
    Signed-off-by: Reinual <michel.launier@videotron.ca>

--- Assets/iCanScript/Engine/Runtime/DataBase/iCS_EditorObject.cs
+++ Assets/iCanScript/Engine/Runtime/DataBase/iCS_EditorObject.cs
@@ -58,6 +58,7 @@ public class iCS_EditorObject {
 		// Commmon
         instance.DisplayOption= toClone.DisplayOption;
         instance.IsNameEditable= toClone.IsNameEditable;
+        instance.LocalPosition= toClone.LocalPosition;
 		// Node
 		instance.MethodName= toClone.MethodName;
 		instance.NbOfParams= toClone.NbOfParams;
