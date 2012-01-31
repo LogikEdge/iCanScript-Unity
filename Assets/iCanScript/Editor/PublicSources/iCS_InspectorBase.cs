7a3c4ffbda29809da6541cfe096c5743da66d93f
Reinual <michel.launier@videotron.ca>
2012-01-31 17:56:29 -0500

    Comment public files to help users install their own nodes.
    Signed-off-by: Reinual <michel.launier@videotron.ca>

--- Assets/iCanScript/Editor/PublicSources/iCS_InspectorBase.cs
+++ Assets/iCanScript/Editor/PublicSources/iCS_InspectorBase.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
 using UnityEditor;
 
+// ===================================================================
+// DO NOT MODIFY THIS FILE.
+// This file is used to invoke the installer at the appropriate time.
+//
+// The installer is used to install new iCanScript nodes in bulk.
+// You can modify the installer to include your own nodes from your
+// classes (see iCS_Installer).
 public class iCS_InspectorBase : iCS_Inspector {
     // ---------------------------------------------------------------
     // Indicates if the installation  has already been executed.
--- Assets/iCanScript/Editor/PublicSources/iCS_Installer.cs
+++ Assets/iCanScript/Editor/PublicSources/iCS_Installer.cs
@@ -2,7 +2,17 @@ using UnityEngine;
 
 public static class iCS_Installer {
     // ------------------------------------------------------------------------
-    // Use this function to install/configure iCanScript.
+    // Use this function to install in bulk iCanScript nodes from your classes.
+    // All public fields, properties, and functions will become available when
+    // installing in bulk.  You can use the Unity or .NET installation files
+    // as a template for your own code (see iCS_NETClasses & iCS_UnityClasses).
+    //
+    // You should consider using the iCanScript custom attributes fro fine
+    // grain control over the installation of your nodes (preferred appraoch).
+    // iCanScript will automatically extract from the assembly your nodes
+    // when custom attributes are used (you do not need to manually add the
+    // calsses).
+    //
     // Currently, we install the Unity & .NET components in the node database.
     public static void Install() {
         iCS_NETClasses.PopulateDataBase();
