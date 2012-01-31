0919a7cb1091e5e3468b74ffca4d3158a57068ba
Reinual <michel.launier@videotron.ca>
2012-01-30 23:46:29 -0500

    Update obsucator for new function names.
    Signed-off-by: Reinual <michel.launier@videotron.ca>

--- Assets/iCanScript/Engine/Runtime/Function/_NET/_NET_Variables.cs
+++ Assets/iCanScript/Engine/Runtime/Function/_NET/_NET_Variables.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 [iCS_Class(Package="Variables")]
-public class CSharp_Variables {
+public class _NET_Variables {
     [iCS_Function(Name="bool")]      public static bool      _int(bool value)        { return value; }
     [iCS_Function(Name="int")]       public static int       _int(int value)         { return value; }
     [iCS_Function(Name="float")]     public static float     _int(float value)       { return value; }
