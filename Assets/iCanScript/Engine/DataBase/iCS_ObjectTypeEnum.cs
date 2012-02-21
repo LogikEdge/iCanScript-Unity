a7324b44ca5ffa9c37d86e747001c4d351bdbf55
Michel Launier <michel.launier@videotron.ca>
2012-02-21 14:14:13 -0500

    Revert "Preparation to refactor port states."
    This reverts commit 724db6d055c8e017d419d107b3fe7066eacb7b78.

--- Assets/iCanScript/Engine/DataBase/iCS_ObjectTypeEnum.cs
+++ Assets/iCanScript/Engine/DataBase/iCS_ObjectTypeEnum.cs
@@ -3,83 +3,24 @@ using System.Collections;
 
 [System.Serializable]
 public enum iCS_ObjectTypeEnum {
-    // Structural nodes
+    // Structural objects
     Behaviour= 0, Module, StateChart, State,
-
-    // Function nodes
+    // Function objects
     Constructor=100,
     InstanceMethod, StaticMethod, 
     InstanceField, StaticField,
     Conversion,
-
-    // Transition nodes
+    // Transition objects
     TransitionModule=200, TransitionGuard, TransitionAction,
-
-    // Data ports
+    // Port object
     InFunctionPort= 300, OutFunctionPort,
     InDynamicModulePort, OutDynamicModulePort,
     InStaticModulePort,  OutStaticModulePort,
-
-	InStaticDataPort, OutStaticDataPort,
-	InDynamicDataPort, OutDynamicDataPort,
-
-	// Transition ports
-    InStatePort= 400, OutStatePort,
-    InTransitionPort, OutTransitionPort,
-
-	// Control-plane ports
-    EnablePort= 500, TriggerPort,
-
+    InStatePort,         OutStatePort,
+    InTransitionPort,    OutTransitionPort,
+    EnablePort,
     // Undefined
     Unknown=1000
-}
-
-public static class iCS_ObjectType {
-	// Nodes
-	public static bool IsNode(iCS_ObjectTypeEnum ot)				{ return IsStructuralNode(ot) || IsFunctionNode(ot) || IsTransitionNode(ot); }
-	
-	// Structural nodes
-	public static bool IsStructuralNode(iCS_ObjectTypeEnum ot)  	{ return IsBehaviour(ot) || IsModule(ot) || IsStateChart(ot) || IsState(ot); }
-	public static bool IsBehaviour(iCS_ObjectTypeEnum ot)       	{ return ot == iCS_ObjectTypeEnum.Behaviour; }
-	public static bool IsModule(iCS_ObjectTypeEnum ot)				{ return ot == iCS_ObjectTypeEnum.Module; }
-	public static bool IsStateChart(iCS_ObjectTypeEnum ot)			{ return ot == iCS_ObjectTypeEnum.StateChart; }
-	public static bool IsState(iCS_ObjectTypeEnum ot)				{ return ot == iCS_ObjectTypeEnum.State; }
-	                                                            	
-	// Function nodes                                           	
-	public static bool IsFunctionNode(iCS_ObjectTypeEnum ot)		{ return IsConstructor(ot) || IsInstanceMethod(ot) || IsStaticMethod(ot) || IsInstanceField(ot) || IsStaticField(ot) || IsConversion(ot); }
-	public static bool IsConstructor(iCS_ObjectTypeEnum ot)			{ return ot == iCS_ObjectTypeEnum.Constructor; }
-	public static bool IsInstanceMethod(iCS_ObjectTypeEnum ot)		{ return ot == iCS_ObjectTypeEnum.InstanceMethod; }
-	public static bool IsStaticMethod(iCS_ObjectTypeEnum ot)		{ return ot == iCS_ObjectTypeEnum.StaticMethod; }
-	public static bool IsInstanceField(iCS_ObjectTypeEnum ot)		{ return ot == iCS_ObjectTypeEnum.InstanceField; }
-	public static bool IsStaticField(iCS_ObjectTypeEnum ot)			{ return ot == iCS_ObjectTypeEnum.StaticField; }
-	public static bool IsConversion(iCS_ObjectTypeEnum ot)			{ return ot == iCS_ObjectTypeEnum.Conversion; }
-                                                                	
-	// Transition nodes                                         	
-	public static bool IsTransitionNode(iCS_ObjectTypeEnum ot)		{ return IsTransitionModule(ot) || IsTransitionGuard(ot) || IsTransitionAction(ot); }
-	public static bool IsTransitionModule(iCS_ObjectTypeEnum ot)	{ return ot == iCS_ObjectTypeEnum.TransitionModule; }
-	public static bool IsTransitionGuard(iCS_ObjectTypeEnum ot)		{ return ot == iCS_ObjectTypeEnum.TransitionGuard; }
-	public static bool IsTransitionAction(iCS_ObjectTypeEnum ot)	{ return ot == iCS_ObjectTypeEnum.TransitionAction; }
-	
-	// Ports
-	public static bool IsPort(iCS_ObjectTypeEnum ot)				{ return IsDataPort(ot); }
-	public static bool IsInputPort(iCS_ObjectTypeEnum ot)			{ return IsInStaticDataPort(ot) || IsInDynamicDataPort(ot); }
-	public static bool IsOutputPort(iCS_ObjectTypeEnum ot)			{ return IsOutStaticDataPort(ot) || IsOutDynamicDataPort(ot); }
-	
-	// Data ports
-	public static bool IsDataPort(iCS_ObjectTypeEnum ot)			{ return IsStaticDataPort(ot) || IsDynamicDataPort(ot); }
-	public static bool IsStaticDataPort(iCS_ObjectTypeEnum ot)		{ return IsInStaticDataPort(ot) || IsOutStaticDataPort(ot); }
-	public static bool IsDynamicDataPort(iCS_ObjectTypeEnum ot)		{ return IsInDynamicDataPort(ot) || IsOutDynamicDataPort(ot); }
-	public static bool IsInStaticDataPort(iCS_ObjectTypeEnum ot)	{ return ot == iCS_ObjectTypeEnum.InStaticDataPort || IsTriggerPort(ot); }
-	public static bool IsOutStaticDataPort(iCS_ObjectTypeEnum ot)	{ return ot == iCS_ObjectTypeEnum.OutStaticDataPort; }
-	public static bool IsInDynamicDataPort(iCS_ObjectTypeEnum ot)	{ return ot == iCS_ObjectTypeEnum.InDynamicDataPort || IsEnablePort(ot); }
-	public static bool IsOutDynamicDataPort(iCS_ObjectTypeEnum ot)	{ return ot == iCS_ObjectTypeEnum.OutDynamicDataPort; }
-	public static bool IsEnablePort(iCS_ObjectTypeEnum ot)			{ return ot == iCS_ObjectTypeEnum.EnablePort; }
-	public static bool IsTriggerPort(iCS_ObjectTypeEnum ot)			{ return ot == iCS_ObjectTypeEnum.TriggerPort; }
-
-	// Transition ports
-	public static bool IsTransitionPort(iCS_ObjectTypeEnum ot)		{ return IsInTransitionPort(ot) || IsOutTransitionPort(ot); }
-	public static bool IsInTransitionPort(iCS_ObjectTypeEnum ot)	{ return ot == iCS_ObjectTypeEnum.InTransitionPort; }
-	public static bool IsOutTransitionPort(iCS_ObjectTypeEnum ot)	{ return ot == iCS_ObjectTypeEnum.OutTransitionPort; }
 }
 
 public static partial class WD {
