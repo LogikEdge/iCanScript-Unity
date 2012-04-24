301bd496d895c1560a383216ea1b056f2778431e
Michel Launier <michel.launier@videotron.ca>
2012-04-24 19:46:01 -0400

    Define Tree view data source interface.

--- Assets/iCanScript/Editor/DisruptiveSoftware/DSTreeViewDataSource.cs
+++ Assets/iCanScript/Editor/DisruptiveSoftware/DSTreeViewDataSource.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 using System.Collections;
 
 public interface DSTreeViewDataSource {
-    int     NumberOfRowsInTreeView(DSTreeView treeView);
-	int		GetObjectParentInTreeView(DSTreeView treeView, int row);
-    Vector2 DisplaySizeForObjectInTreeView(DSTreeView treeView, int row);
-    void    DisplayObjectInTreeView(DSTableView tableView, int row, Rect postion);
-	void	OnMouseDown(DSTreeView treeView, int row);
+	void	Reset();
+	bool	MoveToNext();
+	bool	MoveToNextSibling();
+	bool	MoveToFirstChild();
+	Vector2	CurrentObjectDisplaySize();
+	void	DisplayCurrentObject();
 }
