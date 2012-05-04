a4d679970e2294477989107ff516b7379b47e250
Michel Launier <michel.launier@videotron.ca>
2012-05-04 16:23:47 -0400

    Fix Undo/Redo for DragAndDrop onto graph.

--- Assets/iCanScript/Editor/Core/Editors/GraphEditor/iCS_GraphEditor_DragAndDrop.cs
+++ Assets/iCanScript/Editor/Core/Editors/GraphEditor/iCS_GraphEditor_DragAndDrop.cs
@@ -8,77 +8,72 @@ using System.Collections;
 // ===========================================================================
 public partial class iCS_GraphEditor : iCS_EditorWindow {
     // ======================================================================
-    // Types.
-	// ----------------------------------------------------------------------
-    enum DraggedObjectTypeEnum { Library, Texture, Unknown };
-    
-    // ======================================================================
     // Respond to Unity drag & drop protocol.
 	// ----------------------------------------------------------------------
     void DragAndDropPerform() {
-        // Show a copy icon on the drag
-//        if(GetDraggedObjectType(GetDraggedObject()) != DraggedObjectTypeEnum.Unknown) {
-
-            IStorage.RegisterUndo("DragAndDrop");
-
-            DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
-            DragAndDrop.AcceptDrag();
-//        }        
+        IStorage.RegisterUndo("DragAndDrop");			
+        DragAndDrop.visualMode = DragAndDropVisualMode.Generic;
+        DragAndDrop.AcceptDrag();
     }
 	// ----------------------------------------------------------------------
     void DragAndDropUpdated() {
-        // Show a copy icon on the drag
-//        if(GetDraggedObjectType(GetDraggedObject()) != DraggedObjectTypeEnum.Unknown) {
-            DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
-//        }        
+	    iCS_EditorObject eObj= GetObjectAtMousePosition();
+		if(eObj != null) {
+	        UnityEngine.Object draggedObject= GetDraggedObject();
+	        if(eObj.IsInputPort) {
+	            Type portType= eObj.RuntimeType;
+	            Type dragObjType= draggedObject.GetType();
+	            if(iCS_Types.IsA(portType, dragObjType)) {			
+			    	DragAndDrop.visualMode = DragAndDropVisualMode.Link;
+					return;
+				}
+			}
+			if(eObj.IsNode && draggedObject is Texture) {
+		    	DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+				return;				
+			}
+		}
+    	DragAndDrop.visualMode = DragAndDropVisualMode.Generic;
     }
 	// ----------------------------------------------------------------------
     void DragAndDropExited() {
         UnityEngine.Object draggedObject= GetDraggedObject();
-        if(draggedObject == null) return;
-        switch(GetDraggedObjectType(draggedObject)) {
-            // Paste iCanScript Library into graph.
-            case DraggedObjectTypeEnum.Library: {
-                iCS_Storage storage= GetDraggedLibrary(draggedObject);
-                if(storage != null) {
-                    PasteIntoGraph(ViewportToGraph(MousePosition), storage, storage.EditorObjects[0]);
-                }
-                break;
-            }
-            // Update node iconic representation or initialize an node texture input.
-            case DraggedObjectTypeEnum.Texture: {
-                Texture newTexture= GetDraggedTexture(draggedObject);
-                iCS_EditorObject eObj= GetObjectAtMousePosition();
-                if(eObj.IsPort) {
-                    Type portType= eObj.RuntimeType;
-                    Type dragObjType= draggedObject.GetType();
-                    if(iCS_Types.IsA(portType, dragObjType)) {
-                        UpdatePortInitialValue(eObj, draggedObject);
-                    }
-                } else if(eObj.IsNode) {
-                    string iconGUID= newTexture != null ? AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(newTexture)) : null;
-                    if(newTexture != null) {
-                        eObj.IconGUID= iconGUID;                    
-                        IStorage.Minimize(eObj);
-                        // Remove data so that we don't get called multiple times (Unity bug !!!).
-                        DragAndDrop.objectReferences= new UnityEngine.Object[0];
-                    }
-                }
-                break;
-            }
-            default: {
-                iCS_EditorObject eObj= GetObjectAtMousePosition();
-                if(eObj.IsPort) {
-                    Type portType= eObj.RuntimeType;
-                    Type dragObjType= draggedObject.GetType();
-                    if(iCS_Types.IsA(portType, dragObjType)) {
-                        UpdatePortInitialValue(eObj, draggedObject);
-                    }
-                }
-                break;
-            }
-        }
-        DragAndDrop.PrepareStartDrag();
+		if(draggedObject == null) { return; }
+
+		// Copy/Paste library from prefab
+        iCS_Storage storage= GetDraggedLibrary(draggedObject);
+		if(storage != null) {
+            PasteIntoGraph(ViewportToGraph(MousePosition), storage, storage.EditorObjects[0]);
+			// Remove data so that we don't get called multiple times (Unity bug !!!).
+            DragAndDrop.objectReferences= new UnityEngine.Object[0];
+			return;
+		}
+		
+	    iCS_EditorObject eObj= GetObjectAtMousePosition();
+		if(eObj != null) {
+	        if(eObj.IsInputPort) {
+	            Type portType= eObj.RuntimeType;
+	            Type dragObjType= draggedObject.GetType();
+	            if(iCS_Types.IsA(portType, dragObjType)) {			
+                    UpdatePortInitialValue(eObj, draggedObject);
+					// Remove data so that we don't get called multiple times (Unity bug !!!).
+		            DragAndDrop.objectReferences= new UnityEngine.Object[0];
+					return;
+				}
+			}
+			if(eObj.IsNode && draggedObject is Texture) {
+                Texture newTexture= draggedObject as Texture;
+                string iconGUID= newTexture != null ? AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(newTexture)) : null;
+                if(newTexture != null) {
+                    eObj.IconGUID= iconGUID;                    
+                    IStorage.Minimize(eObj);
+                    // Remove data so that we don't get called multiple times (Unity bug !!!).
+                    DragAndDrop.objectReferences= new UnityEngine.Object[0];
+				}
+				return;				
+			}
+		}
+		Undo.PerformUndo();
     }
 
     // ======================================================================
@@ -87,21 +82,6 @@ public partial class iCS_GraphEditor : iCS_EditorWindow {
     UnityEngine.Object GetDraggedObject() {
         UnityEngine.Object[] draggedObjects= DragAndDrop.objectReferences;
         return draggedObjects.Length >= 1 ? draggedObjects[0] : null;        
-    }
-	// ----------------------------------------------------------------------
-    DraggedObjectTypeEnum GetDraggedObjectType(UnityEngine.Object obj) {
-        if(obj == null) return DraggedObjectTypeEnum.Unknown;
-        // Library
-        if(obj is GameObject) {
-            GameObject go= obj as GameObject;
-            iCS_Storage storage= go.GetComponent<iCS_Library>();
-            return storage != null ? DraggedObjectTypeEnum.Library : DraggedObjectTypeEnum.Unknown;
-        }
-        // Icon
-        if(obj is Texture) {
-            return DraggedObjectTypeEnum.Texture;
-        }
-        return DraggedObjectTypeEnum.Unknown;
     }
 	// ----------------------------------------------------------------------
     iCS_Storage GetDraggedLibrary(UnityEngine.Object draggedObject) {
@@ -113,10 +93,6 @@ public partial class iCS_GraphEditor : iCS_EditorWindow {
             }
         }
         return null;
-    }
-	// ----------------------------------------------------------------------
-    Texture GetDraggedTexture(UnityEngine.Object draggedObject) {
-        return draggedObject != null ? draggedObject as Texture : null;
     }
 
     // ======================================================================
