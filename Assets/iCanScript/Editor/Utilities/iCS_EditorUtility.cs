287dd8e5d2b9873da68683dbecc991496cbab948
Reinual <michel.launier@videotron.ca>
2012-05-25 12:42:03 -0400

    Remove unused code.
    Signed-off-by: Reinual <michel.launier@videotron.ca>

--- Assets/iCanScript/Editor/Utilities/iCS_EditorUtility.cs
+++ Assets/iCanScript/Editor/Utilities/iCS_EditorUtility.cs
@@ -66,6 +66,7 @@ public static class iCS_EditorUtility {
     }
 
     // ======================================================================
+    // Object high-order manipulation.
 	// ----------------------------------------------------------------------
     // Ask the user confirmation to destroy the object.  True is returned
     // if the user has accepted to delete the object; false is returned 
@@ -77,43 +78,6 @@ public static class iCS_EditorUtility {
 
     // ======================================================================
     // GUI helpers
-	// ----------------------------------------------------------------------
-    public static GUIStyle CloneGUIStyle(string name, GUIStyle toClone) {
-        GUIStyle instance= new GUIStyle();
-        instance.name         = name;
-        instance.normal       = CloneGUIStyleState(toClone.normal);
-        instance.hover        = CloneGUIStyleState(toClone.hover);
-        instance.active       = CloneGUIStyleState(toClone.active);
-        instance.focused      = CloneGUIStyleState(toClone.focused);
-        instance.onNormal     = CloneGUIStyleState(toClone.onNormal);
-        instance.onHover      = CloneGUIStyleState(toClone.onHover);
-        instance.onActive     = CloneGUIStyleState(toClone.onActive);
-        instance.onFocused    = CloneGUIStyleState(toClone.onFocused);
-        instance.border       = toClone.border;
-        instance.margin       = toClone.margin;
-        instance.padding      = toClone.padding;
-        instance.overflow     = toClone.overflow;
-        instance.font         = toClone.font;
-        instance.imagePosition= toClone.imagePosition;
-        instance.alignment    = toClone.alignment;
-        instance.wordWrap     = toClone.wordWrap;
-        instance.clipping     = toClone.clipping;
-        instance.contentOffset= toClone.contentOffset;
-        instance.fixedWidth   = toClone.fixedWidth;
-        instance.fontSize     = toClone.fontSize;
-        instance.fontStyle    = toClone.fontStyle;
-        instance.fixedHeight  = toClone.fixedHeight;
-        instance.stretchWidth = toClone.stretchWidth;
-        instance.stretchHeight= toClone.stretchHeight;
-        return instance;
-    }
-	// ----------------------------------------------------------------------
-    public static GUIStyleState CloneGUIStyleState(GUIStyleState toClone) {
-        GUIStyleState instance= new GUIStyleState();
-        instance.textColor= toClone.textColor;
-        instance.background= toClone.background;
-        return instance;
-    }
 	// ----------------------------------------------------------------------
 	public static float GetGUIStyleHeight(GUIStyle style) {
 		float height= style.lineHeight+style.border.vertical;
