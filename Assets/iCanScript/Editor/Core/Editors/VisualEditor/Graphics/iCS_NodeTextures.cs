310209e3781714b19f0b1803d6b018001c764ad4
Michel Launier <michel.launier@videotron.ca>
2012-09-26 19:32:24 -0400

    Prepare for node dynamic rendering into texture.

---
+++ Assets/iCanScript/Editor/Core/Editors/VisualEditor/Graphics/iCS_NodeTextures.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public static class iCS_NodeTextures {
+    // ======================================================================
+    // PROPERTIES
+    // ----------------------------------------------------------------------
+	static Texture2D	myTopCornerTemplate   = null;
+	static Texture2D	myBottomCornerTemplate= null;
+
+    // ----------------------------------------------------------------------
+	static Dictionary<Color,Texture2D>	myTopCornerTextures   = null;
+	static Dictionary<Color,Texture2D>	myBottomCornerTextures= null;
+	
+}
