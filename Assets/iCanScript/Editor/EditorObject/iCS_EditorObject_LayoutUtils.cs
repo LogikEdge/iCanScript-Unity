9e4a19cf3b8d0175fa73cebd320d06846487f77e
Michel Launier <michel.launier@videotron.ca>
2012-11-26 16:59:22 -0500

    Added collision detection algorithm.

--- Assets/iCanScript/Editor/EditorObject/iCS_EditorObject_LayoutUtils.cs
+++ Assets/iCanScript/Editor/EditorObject/iCS_EditorObject_LayoutUtils.cs
@@ -3,6 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 
 public partial class iCS_EditorObject {
+    // Adds a margin around given rectangle ---------------------------------
+    static Rect AddMargins(Rect r) {
+        var m= iCS_Config.MarginSize;
+        var m2= 2f*m;
+        return new Rect(r.x-m, r.y-m, r.width+m2, r.height+m2);
+    }
+    // ----------------------------------------------------------------------
+	// Returns true if a collision exists in the given node array.
+	static bool DoesCollide(iCS_EditorObject[] nodes) {
+		var len= nodes.Length;
+		Rect[] rs= new Rect[len];
+		for(int i= 0; i < len; ++ i) {
+			rs[i]= nodes[i].GlobalRect;
+		}
+		for(int i= 0; i < len-1; ++i) {
+			var r1= AddMargins(rs[i]);
+			for(int j= i+1; j < len; ++j) {
+				if(Math3D.DoesCollide(r1, rs[j])) {
+					return true;
+				}
+			}
+		}
+		return false;
+	} 
+	
     // ----------------------------------------------------------------------
     // Updates the global Rect arround the children nodes.  It is assume that
     // the children have previously been layed out.  The parent node will not
@@ -18,11 +43,18 @@ public partial class iCS_EditorObject {
 		ForEachChildNode(c=> c.GlobalPosition= childPositions[i++]);
     }
 
-    // Adds a margin around given rectangle ---------------------------------
-    public static Rect AddMargins(Rect r) {
-        var m= iCS_Config.MarginSize;
-        var m2= 2f*m;
-        return new Rect(r.x-m, r.y-m, r.width+m2, r.height+m2);
-    }
+    // ----------------------------------------------------------------------
+	Vector2[] BuildListOfRatios(iCS_EditorObject[] children, Rect globalRect) {
+		var len= children.Length;
+		var result= new Vector2[len];
+		if(len == 0) return result;
+		var globalPosition= Math3D.Middle(globalRect);
+		var size= 0.5f*new Vector2(globalRect.width, globalRect.height);
+		for(int i= 0; i < len; ++i) {
+			var localPos= children[i].GlobalPosition-globalPosition;
+			result[i]= new Vector2(localPos.x/size.x, localPos.y/size.y);
+		}
+		return result;
+	}
 
 }
