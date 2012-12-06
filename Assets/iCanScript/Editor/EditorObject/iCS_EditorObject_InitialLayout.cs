832feaae5d202139371840b9b5ca5f32c1d15259
Reinual <michel.launier@videotron.ca>
2012-12-05 19:49:24 -0500

    Draft out the "InitialLayout" function.  To be revised.

--- Assets/iCanScript/Editor/EditorObject/iCS_EditorObject_InitialLayout.cs
+++ Assets/iCanScript/Editor/EditorObject/iCS_EditorObject_InitialLayout.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public partial class iCS_EditorObject {
     // ======================================================================
+    /*
+        TODO: Revise InitialLayout ...
+    */
     void InitialLayout() {
         // Port position will be initialized by the parent node.
         if(IsPort) return;
@@ -10,14 +13,22 @@ public partial class iCS_EditorObject {
         if(!IsVisible) return;
         // Iconized nodes will be initialized by the parent node.
         if(IsIconized) return;
-        // Folded nodes must initialize size and layout its ports using position ratio.
+        // Initialize the node size using the children position ratio.
+        DisplaySize= ComputeNodeSizeFromRatio();
+        // Layout any uninitialized port.
+        InitialPortLayout();
+        // Nothing more to do if we have no visible children.
         if(IsFolded) {
-            DisplaySize= ComputeNodeSizeFromRatio();
-            InitialPortLayout();
             return;
         }
-        /*
-            TODO: initial position for unfolded nodes...
-        */
+        // Update global position of child nodes.
+        var childNodes= BuildListOfChildNodes(c=> !c.IsFloating);
+        var childrenRect= NodeGlobalChildRect;
+        foreach(var cNode in childNodes) {
+            var ratio= cNode.NodePositionRatio;
+            var x= childrenRect.x+ratio.x*childrenRect.width;
+            var y= childrenRect.y+ratio.y*childrenRect.height;
+            cNode.GlobalPosition= new Vector2(x,y);
+        }
     }
 }
