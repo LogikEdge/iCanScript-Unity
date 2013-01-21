976d67bf58c7cb8c3594cdc65c30143980677207
Michel Launier <michel.launier@videotron.ca>
2013-01-21 11:22:10 -0500

    Revert "Beautify iCS_Strings"
    This reverts commit 42aaec9600ae9b8b450c6720d5d6dd541b706983.

--- Assets/iCanScript/Engine/iCS_Strings.cs
+++ Assets/iCanScript/Engine/iCS_Strings.cs
@@ -2,11 +2,9 @@ using UnityEngine;
 using System.Collections;
 
 public class iCS_Strings {
-    // -----------------------------------------------------------------------
     // Common
     public const string This        = "this";
     
-    // -----------------------------------------------------------------------
     // Behaviour Allowed Children
     public const string Start       = "Start";
     public const string Update      = "Update";
@@ -15,27 +13,22 @@ public class iCS_Strings {
     public const string OnGUI       = "OnGUI";
     public const string OnDrawGizmos= "OnDrawGizmos";
     
-    // -----------------------------------------------------------------------
     // State Allowed Children
     public const string OnEntry = "OnEntry";
     public const string OnUpdate= "OnUpdate";
     public const string OnExit  = "OnExit";
     
-    // -----------------------------------------------------------------------
     // Special node names.
     public const string TransitionEntryModule= "TransitionEntry";
     public const string TransitionExitModule = "TransitionExit";
 
-    // -----------------------------------------------------------------------
     // Special port names.
     public const string EnablePort= "enable";
     
-    // -----------------------------------------------------------------------
     // Reflection methods
     public const string AddChildMethod   = "AddChild";
     public const string RemoveChildMethod= "RemoveChild";
     
-    // -----------------------------------------------------------------------
     // Gizmos
     public const string GizmoIcon            = "iCanScriptGizmo.png";
 
