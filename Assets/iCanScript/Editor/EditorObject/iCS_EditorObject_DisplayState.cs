ad1775dda18fe1fe526f8450c8c8eab6099758f9
Reinual <michel.launier@videotron.ca>
2013-01-25 10:49:41 -0500

    Added IsAnimated query functions.

--- Assets/iCanScript/Editor/EditorObject/iCS_EditorObject_DisplayState.cs
+++ Assets/iCanScript/Editor/EditorObject/iCS_EditorObject_DisplayState.cs
@@ -9,6 +9,13 @@ public partial class iCS_EditorObject {
     public bool IsUnfolded          { get { return DisplayOption == iCS_DisplayOptionEnum.Unfolded; }}
     public bool IsFolded            { get { return DisplayOption == iCS_DisplayOptionEnum.Folded;   }}
     public bool IsIconized          { get { return DisplayOption == iCS_DisplayOptionEnum.Iconized; }}
+    public void Iconize()           { DisplayOption= iCS_DisplayOptionEnum.Iconized; IsDirty= true; }
+    public void Fold()              { DisplayOption= iCS_DisplayOptionEnum.Folded;   IsDirty= true; }
+    public void Unfold()            { DisplayOption= iCS_DisplayOptionEnum.Unfolded; IsDirty= true; }
+
+    // ======================================================================
+    // High-order display state functions.
+    // ----------------------------------------------------------------------
     public bool IsVisible {
         get {
             var parent= Parent;
@@ -18,8 +25,34 @@ public partial class iCS_EditorObject {
             return parent.IsVisible;            
         }
     }
-    public void Iconize()           { DisplayOption= iCS_DisplayOptionEnum.Iconized; IsDirty= true; }
-    public void Fold()              { DisplayOption= iCS_DisplayOptionEnum.Folded;   IsDirty= true; }
-    public void Unfold()            { DisplayOption= iCS_DisplayOptionEnum.Unfolded; IsDirty= true;}
-
+    // ----------------------------------------------------------------------
+    public bool IsVisibleWithAnimation {
+        get {
+            var area= Math3D.Area(AnimatedDisplaySize);
+            if(area < 0.1f) return false;
+            return Parent.IsVisibleWithAnimation;
+        }
+    }
+    // ----------------------------------------------------------------------
+    // Returns true if the display size is currently being animated.
+    public bool IsDisplaySizeAnimated {
+        get {
+            return  myAnimatedDisplaySize.IsActive;
+        }
+    }
+    // ----------------------------------------------------------------------
+    // Returns true if the display position is currently being animated.
+    public bool IsDisplayPositionAnimated {
+        get {
+            return  myAnimatedGlobalDisplayPosition.IsActive;            
+        }
+    }
+    // ----------------------------------------------------------------------
+    // Returns true if the display size or position are being animated.
+    public bool IsAnimated {
+        get {
+            if(IsDisplaySizeAnimated) return true;
+            return IsDisplayPositionAnimated;
+        }
+    }
 }
