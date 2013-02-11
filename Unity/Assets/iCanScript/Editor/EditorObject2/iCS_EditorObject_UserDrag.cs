1045860e8de99950d632521612b294f5ae5f1297
Reinual <michel.launier@videotron.ca>
2013-02-11 11:04:09 -0500

    Add mode collision resolution on user drag.  No animation.
    Signed-off-by: Reinual <michel.launier@videotron.ca>

--- Unity/Assets/iCanScript/Editor/EditorObject2/iCS_EditorObject_UserDrag.cs
+++ Unity/Assets/iCanScript/Editor/EditorObject2/iCS_EditorObject_UserDrag.cs
@@ -6,8 +6,11 @@ public partial class iCS_EditorObject {
     // Forces a new position on the object being dragged by the uesr.
     public void UserDragTo(Vector2 newPosition) {
 		if(IsNode) {
+            IsSticky= true;
             SetGlobalAnchorAndLayoutPosition(newPosition);
+            Parent.ResolveCollisionOnChildrenNodes();
             Parent.WrapAroundChildrenNodes();
+            IsSticky= false;
 		} else {
 			Debug.LogWarning("iCanScript: UserDragTo not implemented for ports.");
 		}
