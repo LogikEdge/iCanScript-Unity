a19c46ea2d163f1f816c1fe78627ef779beb9dba
Michel Launier <michel.launier@videotron.ca>
2013-02-12 11:45:10 -0500

    Resolve function nodes moving when repetitively unfolding/iconizing.

--- Unity/Assets/iCanScript/Editor/IStorage/iCS_IStorage_DisplayStates.cs
+++ Unity/Assets/iCanScript/Editor/IStorage/iCS_IStorage_DisplayStates.cs
@@ -12,21 +12,18 @@ public partial class iCS_IStorage {
             return;
         }
         eObj.Fold();
-        ForEachChild(eObj, child=> { if(child.IsPort) child.Unfold(); });
     }
     public void Fold(int id) { if(IsValid(id)) Fold(EditorObjects[id]); }
     // ----------------------------------------------------------------------
     public void Iconize(iCS_EditorObject eObj) {
         if(!eObj.IsNode) return;
         eObj.Iconize();
-        ForEachChild(eObj, child=> { if(child.IsPort) child.Iconize(); });
     }
     public void Iconize(int id) { if(IsValid(id)) Iconize(EditorObjects[id]); }
     // ----------------------------------------------------------------------
     public void Unfold(iCS_EditorObject eObj) {
         if(!eObj.IsNode) return;
         eObj.Unfold();
-        ForEachChild(eObj, child=> { if(child.IsPort) child.Unfold(); });
     }
     public void Unfold(int id) { if(IsValid(id)) Unfold(EditorObjects[id]); }
 
