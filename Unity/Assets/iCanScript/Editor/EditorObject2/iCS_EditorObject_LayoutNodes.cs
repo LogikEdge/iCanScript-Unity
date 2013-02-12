9638762b74ab706e0246e8153bcc5a1280be4f03
Reinual <michel.launier@videotron.ca>
2013-02-12 15:17:50 -0500

    Remove more obsoleted layout functions.
    Signed-off-by: Reinual <michel.launier@videotron.ca>

--- Unity/Assets/iCanScript/Editor/EditorObject2/iCS_EditorObject_LayoutNodes.cs
+++ Unity/Assets/iCanScript/Editor/EditorObject2/iCS_EditorObject_LayoutNodes.cs
@@ -6,11 +6,6 @@ using System.Collections;
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 public partial class iCS_EditorObject {
     // ----------------------------------------------------------------------
-    // Resolve collision between children then wrap node around the children.
-    public void LayoutNodes() {
-        UpdateNodeLayoutSize();
-    }
-    // ----------------------------------------------------------------------
     // Layout the nodes from the parent of the object moving up the hierarchy
     // until we reach the top.  The sticky bit is carried over from the object
     // to the parent.
@@ -18,8 +13,7 @@ public partial class iCS_EditorObject {
         var parent= ParentNode;
         if(parent == null) return;
         var parentGlobalRect= parent.GlobalLayoutRect;
-        parent.ResolveCollisionOnChildrenNodes();
-        parent.WrapAroundChildrenNodes();
+        parent.LayoutNode();
         if(Math3D.IsNotEqual(parentGlobalRect, parent.GlobalLayoutRect)) {
             parent.IsSticky= IsSticky;
             parent.LayoutParentNodesUntilTop();
