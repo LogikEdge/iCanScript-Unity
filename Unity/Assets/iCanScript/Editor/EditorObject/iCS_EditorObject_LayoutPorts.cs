a6de4515839dba0195943ecb8e52719688d4c8b6
Reinual <michel.launier@videotron.ca>
2013-02-18 15:15:23 -0500

    Greatly improved port collision resolution algorithm avoiding infinitesimal adjustments on every iteration.
    Signed-off-by: Reinual <michel.launier@videotron.ca>

--- Unity/Assets/iCanScript/Editor/EditorObject/iCS_EditorObject_LayoutPorts.cs
+++ Unity/Assets/iCanScript/Editor/EditorObject/iCS_EditorObject_LayoutPorts.cs
@@ -1,4 +1,3 @@
-#define NEW_PORT_COLLISION_ALGO
 using UnityEngine;
 using System;
 using System.Collections;
@@ -74,53 +73,6 @@ public partial class iCS_EditorObject {
 			ports[i].LocalLayoutPosition= new Vector2(left+xs[i], y);
 		}
     }
-    // IMPROVE: Remove port shake when user drags.
-#if NEW_PORT_COLLISION_ALGO
-// ----------------------------------------------------------------------
-// Resolves the port layout position for a given edge.
-static float[] ResolvePortCollisions(float[] sortedPosition, float availableLength) {
-    // Nothing collision to resolve if we don't have at least two ports.
-    int nbPorts= sortedPosition.Length;
-    if(nbPorts < 2) return sortedPosition;
-    // We can't resolve collision if we don't have enough space to put all ports.
-    // In such a situation, we simply distribute the ports has best we can on the
-    // available size.
-    float minSeparation= iCS_EditorConfig.MinimumPortSeparation;
-    if(minSeparation*(nbPorts-1) >= availableLength) {
-        var step= availableLength/(nbPorts-1);
-        for(int i= 0; i < nbPorts; ++i) {
-            sortedPosition[i]= i*step;
-        }
-        return sortedPosition;
-    }
-    // We have enough space to resolve the port layout and avoid overlap.
-    for (int i= 0; i < nbPorts-1; ++i) {
-        var iPos= sortedPosition[i];
-        for (int j= nbPorts-1; j != i; --j) {
-            var jPos= sortedPosition[j];
-            var minSep= (j-i)*minSeparation;
-            var separation= jPos-iPos;
-            if(Math3D.IsSmaller(separation, minSep)) {
-                var halfDelta= 0.5f*(minSep-separation);
-                iPos= iPos-halfDelta;
-                jPos= jPos+halfDelta;
-                if(iPos < 0) {
-                    jPos+= -iPos;
-                    iPos= 0;
-                }
-                if(jPos > availableLength) {
-                    iPos-= jPos-availableLength;
-                    jPos= availableLength;
-                }
-                for(int k= 0; k <= (j-i); ++k) {
-                    sortedPosition[i+k]= iPos+k*minSeparation;
-                }
-            }
-        }
-    }
-    return sortedPosition;
-}
-#else
     // ----------------------------------------------------------------------
     // Resolves the port layout position for a given edge.
     static float[] ResolvePortCollisions(float[] sortedPosition, float availableLength) {
@@ -139,48 +91,50 @@ static float[] ResolvePortCollisions(float[] sortedPosition, float availableLeng
             return sortedPosition;
         }
         // We have enough space to resolve the port layout and avoid overlap.
-        // Determine min/max position for each port.
-        float[] minPositions= new float[nbPorts];
-        float[] maxPositions= new float[nbPorts]; 
-        for(int i= 0; i < nbPorts; ++i) {
-            minPositions[i]= i*minSeparation;
-            maxPositions[i]= availableLength-(nbPorts-1-i)*minSeparation;
-            if(Math3D.IsSmaller(sortedPosition[i], minPositions[i])) sortedPosition[i]= minPositions[i];
-            if(Math3D.IsGreater(sortedPosition[i], maxPositions[i])) sortedPosition[i]= maxPositions[i];
-        }
-        // Iterate resolving collisions
-        float kAllowedOverlap= 0.01f;
-		int retry= nbPorts*nbPorts;
-        for(int i= 0; i < nbPorts-1 && retry > 0; ++i) {
-            float overlap= -(sortedPosition[i+1]-sortedPosition[i]-minSeparation);
-            if(Math3D.IsGreater(overlap, kAllowedOverlap)) {
-				if(Math3D.IsEqual(sortedPosition[i], minPositions[i])) {
-                    sortedPosition[i+1]+= overlap;						
-				} else if(Math3D.IsEqual(sortedPosition[i+1], maxPositions[i+1])) {
-                    sortedPosition[i]  -= overlap;
-				} else {
-                    sortedPosition[i]  -= 0.5f*overlap;
-                    sortedPosition[i+1]+= 0.5f*overlap;						
-				}
-	            if(Math3D.IsSmaller(sortedPosition[i], minPositions[i])) sortedPosition[i]= minPositions[i];
-	            if(Math3D.IsGreater(sortedPosition[i], maxPositions[i])) sortedPosition[i]= maxPositions[i];
-	            if(Math3D.IsSmaller(sortedPosition[i+1], minPositions[i+1])) sortedPosition[i+1]= minPositions[i+1];
-	            if(Math3D.IsGreater(sortedPosition[i+1], maxPositions[i+1])) sortedPosition[i+1]= maxPositions[i+1];
-				// Restart from previous
-				--retry;
-				if(i != 0) i-= 2;
-			}
-        }
-        if(retry <= 0) {
-            /*
-                FIXME: Port layout not stabilizing when node is animated.
-            */
-            Debug.LogWarning("iCanScript: Difficulty stabilizing port layout !!!");
+        bool mustRelayout= true;
+        while(mustRelayout) {
+            mustRelayout= false;
+            for (int i= 0; i < nbPorts-1; ++i) {
+                var p1= sortedPosition[i];
+                int j= i+1;
+                var p2= sortedPosition[j];
+                var diff= p2-p1;
+                if(Math3D.IsGreater(diff, minSeparation)) continue;
+                mustRelayout= Math3D.IsSmaller(diff, minSeparation);
+                for (; j < nbPorts-1; ++j) {
+                    p1= p2;
+                    p2= sortedPosition[j+1];
+                    diff= p2-p1;
+                    if(Math3D.IsGreater(diff, minSeparation)) break;
+                    if(Math3D.IsSmaller(diff, minSeparation)) {
+                        mustRelayout= true;
+                    }
+                }
+                if(mustRelayout) {
+                    var iPos= sortedPosition[i];
+                    var jPos= sortedPosition[j];
+                    diff= sortedPosition[j]-sortedPosition[i];
+                    var minSep= (j-i)*minSeparation;
+                    var halfDelta= 0.5f*(minSep-diff);
+                    iPos= iPos-halfDelta;
+                    jPos= jPos+halfDelta;
+                    if(iPos < 0) {
+                        jPos+= -iPos;
+                        iPos= 0;
+                    }
+                    if(jPos > availableLength) {
+                        iPos-= jPos-availableLength;
+                    }
+                    for(int k= 0; k <= (j-i); ++k) {
+                        sortedPosition[i+k]= iPos+k*minSeparation;
+                    }
+                    break;
+                }
+            }        
         }
         return sortedPosition;
     }
-#endif
-
+    
     // ======================================================================
     // Port ordering utilities.
     // ----------------------------------------------------------------------
