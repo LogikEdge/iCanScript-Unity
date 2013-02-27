4ef15e286e00c18ffef06f100750ce2952c780c1
Reinual <michel.launier@videotron.ca>
2013-02-27 11:05:28 -0500

    Code: Better local name for source port in auto create instance when dragging port.
    Signed-off-by: Reinual <michel.launier@videotron.ca>

--- Unity/Assets/iCanScript/Editor/Core/Editors/VisualEditor/iCS_VisualEditor_ObjectDrag.cs
+++ Unity/Assets/iCanScript/Editor/Core/Editors/VisualEditor/iCS_VisualEditor_ObjectDrag.cs
@@ -608,12 +608,12 @@ public partial class iCS_VisualEditor : iCS_EditorBase {
     }
 	// ----------------------------------------------------------------------
     void AutocreateInstanceNode(Vector2 globalPosition, iCS_EditorObject newParent) {
-        var dragPort= DragFixPort;
-        Type instanceType= dragPort.RuntimeType;
+        var sourcePort= DragFixPort;
+        Type instanceType= sourcePort.RuntimeType;
         if(iCS_Types.IsStaticClass(instanceType)) return;
         if(DragOriginalPort != DragFixPort) return;
         var instance= IStorage.CreateModule(newParent.InstanceId, globalPosition, "", iCS_ObjectTypeEnum.Module, instanceType);
         var thisPort= IStorage.ClassModuleGetInputThisPort(instance);
-        IStorage.SetSource(thisPort, dragPort);
+        IStorage.SetSource(thisPort, sourcePort);
     }
 }
