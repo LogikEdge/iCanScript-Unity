e6dc7af8ef41277133ccbf08394c8c334e9c421e
Michel Launier <michel.launier@videotron.ca>
2013-03-26 10:19:19 -0400

    Avoid to continuously reduce priority of sibling nodes each time the same drag node is moved.

--- Unity/Assets/iCanScript/Editor/EditorObject/iCS_EditorObject_UserDrag.cs
+++ Unity/Assets/iCanScript/Editor/EditorObject/iCS_EditorObject_UserDrag.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 using System.Collections;
 
 public partial class iCS_EditorObject {
+	// ======================================================================
+	// Fields;
+    // ----------------------------------------------------------------------
+	static iCS_EditorObject ourLastDraggedNode= null;
+	
     // ----------------------------------------------------------------------
     // Forces a new position on the object being dragged by the uesr.
     public void NodeDragTo(Vector2 newPosition) {
@@ -9,7 +14,7 @@ public partial class iCS_EditorObject {
             StopAnimation();
             AnchorPosition= newPosition;
             LocalOffset= Vector2.zero;
-			SetAsHighestLayoutPriority();
+			SetDragNodeLayoutPriority();
 			LayoutUnfoldedParentNodesUsingAnimatedChildren();
 		} else {
 			Debug.LogWarning("iCanScript: UserDragTo not implemented for ports.");
@@ -22,9 +27,16 @@ public partial class iCS_EditorObject {
             StopAnimation();
             AnchorPosition= newPosition;
             LocalOffset= Vector2.zero;
+			SetDragNodeLayoutPriority();
 		} else {
 			Debug.LogWarning("iCanScript: UserDragTo not implemented for ports.");
 		}
     }
-
+    // ----------------------------------------------------------------------
+	void SetDragNodeLayoutPriority() {
+		if(ourLastDraggedNode != this) {
+			ourLastDraggedNode= this;
+			SetAsHighestLayoutPriority();
+		}
+	} 
 }
