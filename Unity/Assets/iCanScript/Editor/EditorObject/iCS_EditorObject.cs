1d424f5e6bb26197e5ddd83441c51624ed94ef2e
Michel Launier <michel.launier@videotron.ca>
2013-03-26 11:29:11 -0400

    Avoid using GUI on rebuilding editor objects from engine objects.

--- Unity/Assets/iCanScript/Editor/EditorObject/iCS_EditorObject.cs
+++ Unity/Assets/iCanScript/Editor/EditorObject/iCS_EditorObject.cs
@@ -296,7 +296,9 @@ public partial class iCS_EditorObject {
             iCS_EditorObject editorObj= null;
 		    if(iStorage.EngineObjects[i].InstanceId != -1) {
 		        editorObj= new iCS_EditorObject(i, iStorage);
-				editorObj.myAnchorCache= editorObj.LocalAnchorPosition;
+				if(editorObj.IsNode) {
+					editorObj.myAnchorCache= editorObj.LocalAnchorPosition;					
+				}
 		    }
 	        iStorage.EditorObjects.Add(editorObj);
 		}
