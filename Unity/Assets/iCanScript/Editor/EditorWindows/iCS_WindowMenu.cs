d7c49c59dd3271ea336afad21b736440990ea6e1
Reinual <michel.launier@videotron.ca>
2013-04-02 15:57:20 -0400

    Reduce complexity of user visible files.
    Signed-off-by: Reinual <michel.launier@videotron.ca>

--- Unity/Assets/iCanScript/Editor/EditorWindows/iCS_WindowMenu.cs
+++ Unity/Assets/iCanScript/Editor/EditorWindows/iCS_WindowMenu.cs
@@ -6,14 +6,14 @@ public static class iCS_WindowMenu {
 	// iCanScript Graph editor.
 	[MenuItem("Window/iCanScript/Preferences")]
 	public static void MenuPreferences() {
-        iCS_PreferencesEditorWindow editor= EditorWindow.CreateInstance<iCS_PreferencesEditorWindow>();
+        var editor= EditorWindow.CreateInstance<iCS_PreferencesEditorWindow>();
         editor.ShowUtility();
 	}
     // ======================================================================
 	// iCanScript Graph editor.
 	[MenuItem("Window/iCanScript/Visual Editor")]
 	public static void MenuGraphEditor() {
-        iCS_VisualEditorWindow editor= EditorWindow.GetWindow(typeof(iCS_VisualEditorWindow), false, "Visual Editor") as iCS_VisualEditorWindow;
+        var editor= EditorWindow.GetWindow(typeof(iCS_VisualEditorWindow), false, "Visual Editor");
         EditorWindow.DontDestroyOnLoad(editor);
         editor.hideFlags= HideFlags.DontSave;
 	}
@@ -21,7 +21,7 @@ public static class iCS_WindowMenu {
  	// iCanScript Hierarchy editor Menu.
  	[MenuItem("Window/iCanScript/Hierarchy")]
  	public static void MenuHierarchyEditor() {
-        iCS_HierarchyEditorWindow editor= EditorWindow.GetWindow(typeof(iCS_HierarchyEditorWindow), false, "iCS Hierarchy") as iCS_HierarchyEditorWindow;
+        var editor= EditorWindow.GetWindow(typeof(iCS_HierarchyEditorWindow), false, "iCS Hierarchy");
         EditorWindow.DontDestroyOnLoad(editor);
         editor.hideFlags= HideFlags.DontSave;
  	}
@@ -29,7 +29,7 @@ public static class iCS_WindowMenu {
  	// iCanScript Project editor Menu.
  	[MenuItem("Window/iCanScript/Library")]
  	public static void MenuLibraryEditor() {
-        iCS_LibraryEditorWindow editor= EditorWindow.GetWindow(typeof(iCS_LibraryEditorWindow), false, "iCS Library") as iCS_LibraryEditorWindow;
+        var editor= EditorWindow.GetWindow(typeof(iCS_LibraryEditorWindow), false, "iCS Library");
         EditorWindow.DontDestroyOnLoad(editor);
         editor.hideFlags= HideFlags.DontSave;
 
@@ -38,7 +38,7 @@ public static class iCS_WindowMenu {
  	// iCanScript ClassWizard editor Menu.
  	[MenuItem("Window/iCanScript/Instance Wizard")]
  	public static void MenuInstanceEditor() {
-        iCS_InstanceEditorWindow editor= EditorWindow.GetWindow(typeof(iCS_InstanceEditorWindow), false, "iCS Instance") as iCS_InstanceEditorWindow;
+        var editor= EditorWindow.GetWindow(typeof(iCS_InstanceEditorWindow), false, "iCS Instance");
         EditorWindow.DontDestroyOnLoad(editor);
         editor.hideFlags= HideFlags.DontSave;
  	}
