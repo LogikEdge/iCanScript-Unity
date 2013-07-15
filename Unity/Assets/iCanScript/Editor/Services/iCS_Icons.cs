7d44e191fdeb32a419050a48157612d7d1cf7fa9
Reinual <michel.launier@videotron.ca>
2013-07-15 15:34:07 -0400

    Prepare to support utility icons in the icons service.
    Signed-off-by: Reinual <michel.launier@videotron.ca>

--- Unity/Assets/iCanScript/Editor/Services/iCS_Icons.cs
+++ Unity/Assets/iCanScript/Editor/Services/iCS_Icons.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEditor;
 using System.Collections;
 
-public enum iCS_DefaultIcons {
+public enum iCS_DefaultNodeIcons {
     Company, Library, Behaviour,
     Package, Message, ObjectInstance,
     Function, Variable,
@@ -48,44 +48,44 @@ public static class iCS_Icons {
         if(obj.IsBehaviour) {
             
         } else if(obj.IsMessage) {
-            return GetDefaultNodeIconFor(iCS_DefaultIcons.Message);            
+            return GetDefaultNodeIconFor(iCS_DefaultNodeIcons.Message);            
         } else if(obj.IsKindOfFunction) {
-            return GetDefaultNodeIconFor(iCS_DefaultIcons.Function);
+            return GetDefaultNodeIconFor(iCS_DefaultNodeIcons.Function);
         } else if(obj.IsStateChart) {
-            return GetDefaultNodeIconFor(iCS_DefaultIcons.StateChart);            
+            return GetDefaultNodeIconFor(iCS_DefaultNodeIcons.StateChart);            
         } else if(obj.IsEntryState) {
-            return GetDefaultNodeIconFor(iCS_DefaultIcons.EntryState);            
+            return GetDefaultNodeIconFor(iCS_DefaultNodeIcons.EntryState);            
         } else if(obj.IsState) {
-            return GetDefaultNodeIconFor(iCS_DefaultIcons.State);
+            return GetDefaultNodeIconFor(iCS_DefaultNodeIcons.State);
         }
-        return GetDefaultNodeIconFor(iCS_DefaultIcons.Package);
+        return GetDefaultNodeIconFor(iCS_DefaultNodeIcons.Package);
     }
 
     // -------------------------------------------------------------------------
-    public static Texture2D GetDefaultNodeIconFor(iCS_DefaultIcons iconType) {
+    public static Texture2D GetDefaultNodeIconFor(iCS_DefaultNodeIcons iconType) {
         Texture2D icon= null;
         switch(iconType) {
-            case iCS_DefaultIcons.Company:
+            case iCS_DefaultNodeIcons.Company:
                 icon= iCS_TextureCache.GetIcon(kCompanyIcon); break;
-            case iCS_DefaultIcons.Library:
+            case iCS_DefaultNodeIcons.Library:
                 icon= iCS_TextureCache.GetIcon(kLibraryIcon); break;
-            case iCS_DefaultIcons.Behaviour:
+            case iCS_DefaultNodeIcons.Behaviour:
                 icon= iCS_TextureCache.GetIcon(kBehaviourIcon); break;
-            case iCS_DefaultIcons.Package:
+            case iCS_DefaultNodeIcons.Package:
                 icon= iCS_TextureCache.GetIcon(kPackageIcon); break;
-            case iCS_DefaultIcons.Message:
+            case iCS_DefaultNodeIcons.Message:
                 icon= iCS_TextureCache.GetIcon(kMessageIcon); break;
-            case iCS_DefaultIcons.ObjectInstance:
+            case iCS_DefaultNodeIcons.ObjectInstance:
                 icon= iCS_TextureCache.GetIcon(kObjectInstanceIcon); break;
-            case iCS_DefaultIcons.Function:
+            case iCS_DefaultNodeIcons.Function:
                 icon= iCS_TextureCache.GetIcon(kFunctionIcon); break;
-            case iCS_DefaultIcons.Variable:
+            case iCS_DefaultNodeIcons.Variable:
                 icon= iCS_TextureCache.GetIcon(kVariableIcon); break;
-            case iCS_DefaultIcons.StateChart:
+            case iCS_DefaultNodeIcons.StateChart:
                 icon= iCS_TextureCache.GetIcon(kStateChartIcon); break;
-            case iCS_DefaultIcons.State:
+            case iCS_DefaultNodeIcons.State:
                 icon= iCS_TextureCache.GetIcon(kStateIcon); break;
-            case iCS_DefaultIcons.EntryState:
+            case iCS_DefaultNodeIcons.EntryState:
                 icon= iCS_TextureCache.GetIcon(kEntryStateIcon); break;
         }
         return icon == null ? iCS_TextureCache.GetIcon(kPackageIcon) : icon;
