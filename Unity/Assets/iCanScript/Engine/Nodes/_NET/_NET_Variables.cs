d983e1dea899bc47a081e405ab62aa2305fccfd9
Reinual <michel.launier@videotron.ca>
2013-11-04 02:35:58 -0500

    Moved variables & game controller into base package.
    Signed-off-by: Reinual <michel.launier@videotron.ca>

---
+++ Unity/Assets/iCanScript/Engine/Nodes/_NET/_NET_Variables.cs
@@ -0,0 +1,7 @@
+[iCS_Class(Company="NET",Icon="iCS_LibraryIcon_32x32.png")]
+public static class iCS_Variables {
+    [iCS_Function(Name="bool")]      public static bool      _bool  (bool value)    { return value; }
+    [iCS_Function(Name="int")]       public static int       _int   (int value)     { return value; }
+    [iCS_Function(Name="float")]     public static float     _float (float value)   { return value; }
+    [iCS_Function(Name="string")]    public static string    _string(string value)  { return value; }
+}
