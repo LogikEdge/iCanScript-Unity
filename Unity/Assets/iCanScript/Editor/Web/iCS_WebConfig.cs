70809799c759f67956300084a5207c03a895d902
Reinual <michel.launier@videotron.ca>
2014-02-03 18:13:39 -0500

    Update the software update server information path.

--- Unity/Assets/iCanScript/Editor/Web/iCS_WebConfig.cs
+++ Unity/Assets/iCanScript/Editor/Web/iCS_WebConfig.cs
@@ -5,8 +5,9 @@ public static class iCS_WebConfig {
     // ======================================================================
     public const string URL                  = "www.icanscript.com";
 	public const string ResourcesPath	     = URL+"/resources";
-	public const string VersionsInfoPath     = ResourcesPath+"/versions-info";
-	public const string LatestVersionInfoFile= VersionsInfoPath+"/latest-version-info.json";
+	public const string ReleasesPath		 = ResourcesPath+"/releases";
+	public const string LatestReleasePath	 = ReleasesPath+"/latest";
+	public const string LatestVersionInfoFile= LatestReleasePath+"/version-info.json";
 	public const string SupportPath			 = URL+"/support";
 	public const string DownloadsPage	     = SupportPath+"/downloads";
     public const string PerlScriptPath       = ResourcesPath+"/pl_scripts";
--- Unity/Assets/iCanScript/Engine/iCS_BuildInfo.cs
+++ Unity/Assets/iCanScript/Engine/iCS_BuildInfo.cs
@@ -1,3 +1,3 @@
 public static class iCS_BuildInfo {
-public const string kBuildDateStr= "14/02/03 10:23:10";
+public const string kBuildDateStr= "14/02/03 18:11:39";
 }
