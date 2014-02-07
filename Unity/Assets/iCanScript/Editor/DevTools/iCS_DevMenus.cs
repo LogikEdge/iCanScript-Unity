18f866cf9384413bbf7d0c78025c56e493c09a6c
Reinual <michel.launier@videotron.ca>
2014-02-07 05:15:36 -0500

    Add DevTools folder that is excluded from the staging build.
    Signed-off-by: Reinual <michel.launier@videotron.ca>

--- Unity/Assets/iCanScript/Editor/DevTools/iCS_DevMenus.cs
+++ Unity/Assets/iCanScript/Editor/DevTools/iCS_DevMenus.cs
@@ -1,13 +1,17 @@
-﻿#define DEBUG
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEditor;
 using System.Collections;
 
-#if DEBUG
-public static class iCS_DevelopmentMenu {
+public static class iCS_DevMenu {
+    // ======================================================================
+    // Visual Editor Snapshot
+	[MenuItem("DevTools/Visual Editor Snapshot",false,1000)]
+	public static void MenuVisualEditorSnapshot() {
+		
+	}
     // ======================================================================
     // Sanity Check
-	[MenuItem("DEVELOPMENT/Sanity Check Selection",false,1000)]
+	[MenuItem("DevTools/Sanity Check Selection",false,1020)]
 	public static void MenuSanityCheck() {
 		iCS_IStorage storage= iCS_StorageMgr.IStorage;
 		if(storage == null) return;
@@ -17,9 +21,8 @@ public static class iCS_DevelopmentMenu {
 	}
     // ======================================================================
     // Trigger Periodic Software Update Verification
-	[MenuItem("DEVELOPMENT/Invoke Periodic Software Update Verification",false,1001)]
+	[MenuItem("DevTools/Invoke Periodic Software Update Verification",false,1021)]
 	public static void MenuPeriodicSoftwareUpdateVerification() {
 		iCS_SoftwareUpdateController.PeriodicUpdateVerification();
 	}	
 }
-#endif
--- Unity/Assets/iCanScript/Engine/iCS_BuildInfo.cs
+++ Unity/Assets/iCanScript/Engine/iCS_BuildInfo.cs
@@ -1,3 +1,3 @@
 public static class iCS_BuildInfo {
-public const string kBuildDateStr= "14/02/06 02:55:33";
+public const string kBuildDateStr= "14/02/07 04:37:41";
 }
