039e1e8b5c036b0b316d34f70ca13f3839364114
Reinual <michel.launier@videotron.ca>
2014-03-02 16:03:58 -0500

    Increase version to v1.0.4

--- Unity/Assets/iCanScript/Engine/iCS_Config.cs
+++ Unity/Assets/iCanScript/Engine/iCS_Config.cs
@@ -6,7 +6,7 @@ public static class iCS_Config {
     // Product descriptions. 
 	public const uint   MajorVersion = 1;
 	public const uint   MinorVersion = 0;
-	public const uint   BugFixVersion= 3;
+	public const uint   BugFixVersion= 4;
     public const string ProductName= "iCanScript";
     public const string ProductAcronym= "iCS";
 	public const string ProductPrefix= ProductAcronym+"_";
