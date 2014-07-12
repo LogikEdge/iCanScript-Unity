e637ba659c46b1dd3294002a55015324b1ef3174
Reinual <michel.launier@videotron.ca>
2014-07-11 22:59:06 -0400

    Added TimeDelay function.

---
+++ Unity/Assets/iCanScript/Engine/Nodes/iCanScript/iCS_TimeDelay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[iCS_Class(Company="iCanScript", Icon= "iCS_ClockIcon.psd")]
+public class iCS_TimeDelay {
+    bool    myState= false;
+    float   myElapseTime= 0;
+    
+    [iCS_Function]
+    public iCS_TimeDelay() {
+        myElapseTime= Time.time-1f;
+    }
+    [iCS_Function]
+    public bool Delay(float delayTime, bool trigger) {
+        // Restart delay.
+        if(myState == false && trigger == true) {
+            myElapseTime= Time.time+delayTime;
+        }
+        myState= trigger;
+        return Time.time <= myElapseTime;
+    }
+}
