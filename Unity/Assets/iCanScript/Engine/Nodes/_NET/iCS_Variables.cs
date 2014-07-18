5d359fcb94a25175db838b77ac19fedcb44f8de1
Reinual <michel.launier@videotron.ca>
2014-07-17 23:32:39 -0400

    Added base math for Bool, Int, and Float variables.

--- Unity/Assets/iCanScript/Engine/Nodes/_NET/iCS_Variables.cs
+++ Unity/Assets/iCanScript/Engine/Nodes/_NET/iCS_Variables.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 [iCS_Class(Company="NET",Icon="iCS_LibraryIcon_32x32.png")]
 public static class iCS_Variables {
     [iCS_Function(Name="bool")]      public static bool      _bool  (bool value)    { return value; }
@@ -15,8 +17,14 @@ public struct Bool {
         [iCS_Function] get { return myValue; }
         [iCS_Function] set { myValue= value; }
     }
+    public bool Not {
+        [iCS_Function] get { return !myValue; }
+    }
     
     [iCS_Function] public Bool(bool init= false) { myValue= init; }
+    [iCS_Function] public bool And(bool v) { return myValue & v; }
+    [iCS_Function] public bool Or(bool v)  { return myValue | v; }
+    [iCS_Function] public bool Xor(bool v) { return myValue ^ v; }
 }
 
 [iCS_Class(Company="iCanScript",Library="Variables")]
@@ -27,8 +35,21 @@ public struct Int {
         [iCS_Function] get { return myValue; }
         [iCS_Function] set { myValue= value; }
     }
+    public int Negate {
+        [iCS_Function] get { return -myValue; }
+    }
+    public int Abs {
+        [iCS_Function] get { return myValue < 0 ? -myValue : myValue; }
+    }
+    public int Sign {
+        [iCS_Function] get { return myValue < 0 ? -1 : 1; }
+    }
     
     [iCS_Function] public Int(int init= 0) { myValue= init; }
+    [iCS_Function(Return="a+b")] public int Add(int b) { return myValue+b; }
+    [iCS_Function(Return="a-b")] public int Sub(int b) { return myValue-b; }
+    [iCS_Function(Return="a*b")] public int Mul(int b) { return myValue*b; }
+    [iCS_Function(Return="a/b")] public int Div(int b) { return myValue/b; }
 }
 
 [iCS_Class(Company="iCanScript",Library="Variables")]
@@ -39,7 +60,20 @@ public struct Float {
         [iCS_Function] get { return myValue; }
         [iCS_Function] set { myValue= value; }
     }
+    public float Negate {
+        [iCS_Function] get { return -myValue; }
+    }
+    public float Abs {
+        [iCS_Function] get { return Mathf.Abs(myValue); }
+    }
+    public float Sign {
+        [iCS_Function] get { return Mathf.Sign(myValue); }
+    }
     
     [iCS_Function] public Float(float init= 0f) { myValue= init; }
+    [iCS_Function(Return="a+b")] public float Add(float b) { return myValue+b; }
+    [iCS_Function(Return="a-b")] public float Sub(float b) { return myValue-b; }
+    [iCS_Function(Return="a*b")] public float Mul(float b) { return myValue*b; }
+    [iCS_Function(Return="a/b")] public float Div(float b) { return myValue/b; }
 }
 
