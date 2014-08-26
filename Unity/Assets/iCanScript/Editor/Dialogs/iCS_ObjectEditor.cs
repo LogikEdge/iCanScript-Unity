a1180e0b239b06831830ef1bf07c3eb37b208092
Reinual <michel.launier@videotron.ca>
2014-08-26 14:22:27 -0400

    Define the Object Editor framework to move the Inspector inside the visual editor.

---
+++ Unity/Assets/iCanScript/Editor/Dialogs/iCS_ObjectEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class iCS_ObjectEditor : EditorWindow {
+    // ======================================================================
+    // Initialization/Teardown
+    // ----------------------------------------------------------------------
+    public void Init(Vector2 pos, string _title) {
+        title= _title;
+        position= new Rect(pos.x, pos.y, 300, 200);
+        ShowAuxWindow();
+    }
+    
+    public void OnEnable() {}
+    public void OnDisable() {}
+
+    // ======================================================================
+    // GUI Update
+    // ----------------------------------------------------------------------
+    public void OnGUI() {
+    }
+}
+
