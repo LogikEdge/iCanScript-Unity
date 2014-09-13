2d00141a899fc139ba569a950164fbdd8a971640
Michel Launier <michel.launier@videotron.ca>
2014-09-13 10:46:28 -0400

    Move Timer functionality into Timer class

---
+++ Unity/Assets/iCanScript/Engine/Nodes/iCanScript/iCS_Timer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[iCS_Class(Company="iCanScript", Icon= "iCS_ClockIcon.psd")]
+public static class iCS_Timer {
+    [iCS_Function(Return="elapseTime")]
+    public static float ComputeElapseTime(float time) {
+        return Time.time+time;
+    }
+    [iCS_Function(Return="elapseTime")]
+    public static float StartTimer(float time) {
+        return Time.time+time;
+    }
+    [iCS_Function(Return="isElapsed")]
+    public static bool IsTimeElapsed(float elapseTime, out bool isNotElapsed) {
+        bool isElapsed= Time.time >= elapseTime;
+        isNotElapsed= !isElapsed;
+        return isElapsed;
+    }
+}
