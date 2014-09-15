f592de134743f8caaac88e2ef6e993f4539361af
Michel Launier <michel.launier@videotron.ca>
2014-09-14 20:21:52 -0400

    Rename "elapseTime" to "endTime".

--- Unity/Assets/iCanScript/Engine/Nodes/iCanScript/iCS_Timer.cs
+++ Unity/Assets/iCanScript/Engine/Nodes/iCanScript/iCS_Timer.cs
@@ -3,18 +3,19 @@ using System.Collections;
 
 [iCS_Class(Company="iCanScript", Icon= "iCS_ClockIcon.psd")]
 public static class iCS_Timer {
-    [iCS_Function(Return="elapseTime")]
-    public static float ComputeElapseTime(float time) {
-        return Time.time+time;
-    }
-    [iCS_Function(Return="elapseTime")]
+    [iCS_Function(Return="endTime")]
     public static float StartTimer(float time) {
         return Time.time+time;
     }
     [iCS_Function(Return="isElapsed")]
-    public static bool IsTimeElapsed(float elapseTime, out bool isNotElapsed) {
-        bool isElapsed= Time.time >= elapseTime;
+    public static bool IsTimeElapsed(float endTime, out bool isNotElapsed) {
+        bool isElapsed= Time.time >= endTime;
         isNotElapsed= !isElapsed;
         return isElapsed;
+    }
+    [iCS_Function(Return="remainingTime")]
+    public static float RemainingTime(float endTime) {
+        var remainingTime= Time.time-endTime;
+        return remainingTime > 0 ? remainingTime : 0f; 
     }
 }
