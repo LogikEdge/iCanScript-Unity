f7ae552e399d2e8029526898075d1d06d15f04d7
Michel Launier <michel.launier@videotron.ca>
2014-09-23 11:29:31 -0400

    Untested implementation of the User Function Proxy/Call

--- Unity/Assets/iCanScript/Engine/ExecutionService/iCS_UserFunctionProxy.cs
+++ Unity/Assets/iCanScript/Engine/ExecutionService/iCS_UserFunctionProxy.cs
@@ -21,15 +21,62 @@ public class iCS_UserFunctionProxy : iCS_ActionWithSignature {
     // Execution
     // ----------------------------------------------------------------------
     protected override void DoExecute(int frameId) {
-        // Wait until all inputs are ready.
-        var end= ParametersEnd;
-        for(int i= ParametersStart; i <= end; ++i) {
-            if(IsParameterReady(i, frameId) == false) {
-                return;
+//#if UNITY_EDITOR
+        try {
+//#endif
+            // Wait until all inputs are ready.
+            var parameterStart= ParametersStart;
+            var parameterEnd= ParametersEnd;
+            for(int i= parameterStart; i <= parameterEnd; ++i) {
+                if(IsParameterReady(i, frameId) == false) {
+                    return;
+                }
             }
+            // Fetch all parameters.
+            for(int i= parameterStart; i <= parameterEnd; ++i) {
+                UpdateParameter(i);
+            }
+            // Copy input ports
+            var parameters= Parameters;
+            var userActionParameters= myUserAction.Parameters;
+            for(int i= parameterStart; i < parameterEnd; ++i) {
+                userActionParameters[i]= parameters[i];
+            }
+            // Execute associated function.
+            // TODO: Should desynchronize frameid to force the execution.
+            myUserAction.Execute(frameId);
+            // Copy output ports
+            for(int i= parameterStart; i < parameterEnd; ++i) {
+                parameters[i]= userActionParameters[i];
+            }
+            ReturnValue= myUserAction.ReturnValue;
+            // Reflection the action run status.
+            IsStalled= myUserAction.IsStalled;
+            if(myUserAction.DidExecute(frameId)) {
+                MarkAsExecuted(frameId);
+            }
+            else if(myUserAction.IsCurrent(frameId)){
+                MarkAsCurrent(frameId);
+            }            
+//#if UNITY_EDITOR
         }
-        // Execute associated function.
-        DoForceExecute(frameId);
+        catch(Exception e) {
+            Debug.LogWarning("iCanScript: Exception throw in  "+FullName+" => "+e.Message);
+            string thisName= (InInstance == null ? "null" : InInstance.ToString());
+            string parametersAsStr= "";
+            int nbOfParams= Parameters.Length;
+            if(nbOfParams != 0) {
+                for(int i= 0; i < nbOfParams; ++i) {
+                    parametersAsStr+= Parameters[i].ToString();
+                    if(i != nbOfParams-1) {
+                        parametersAsStr+=", ";
+                    }
+                }
+            }
+            Debug.LogWarning("iCanScript: while invoking => "+thisName+"."+Name+"("+parametersAsStr+")");
+            MarkAsCurrent(frameId);
+        }
+//#endif
     }
     // ----------------------------------------------------------------------
     // TODO: UserFunction.DoForceExecute()
@@ -37,15 +84,40 @@ public class iCS_UserFunctionProxy : iCS_ActionWithSignature {
 //#if UNITY_EDITOR
         try {
 //#endif
+            // Wait until all inputs are ready.
+            var parameterStart= ParametersStart;
+            var parameterEnd= ParametersEnd;
+            for(int i= parameterStart; i <= parameterEnd; ++i) {
+                if(IsParameterReady(i, frameId) == false) {
+                    return;
+                }
+            }
             // Fetch all parameters.
-            var end= ParametersEnd;
-            for(int i= ParametersStart; i <= end; ++i) {
+            for(int i= parameterStart; i <= parameterEnd; ++i) {
                 UpdateParameter(i);
             }
-            
-            // Execute function
-//            ReturnValue= myMethodBase.Invoke(InInstance, Parameters);            
-            MarkAsExecuted(frameId);
+            // Copy input ports
+            var parameters= Parameters;
+            var userActionParameters= myUserAction.Parameters;
+            for(int i= parameterStart; i < parameterEnd; ++i) {
+                userActionParameters[i]= parameters[i];
+            }
+            // Execute associated function.
+            // TODO: Should desynchronize frameid to force the execution.
+            myUserAction.ForceExecute(frameId);
+            // Copy output ports
+            for(int i= parameterStart; i < parameterEnd; ++i) {
+                parameters[i]= userActionParameters[i];
+            }
+            ReturnValue= myUserAction.ReturnValue;
+            // Reflection the action run status.
+            IsStalled= myUserAction.IsStalled;
+            if(myUserAction.DidExecute(frameId)) {
+                MarkAsExecuted(frameId);
+            }
+            else if(myUserAction.IsCurrent(frameId)){
+                MarkAsCurrent(frameId);
+            }            
 //#if UNITY_EDITOR
         }
         catch(Exception e) {
