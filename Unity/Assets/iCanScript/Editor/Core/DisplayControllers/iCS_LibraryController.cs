961dd780fefb24ab991d7ccef78753ddbc3f4c31
Michel Launier <michel.launier@videotron.ca>
2014-10-03 13:04:33 -0400

    Don't search in company & package names

--- Unity/Assets/iCanScript/Editor/Core/DisplayControllers/iCS_LibraryController.cs
+++ Unity/Assets/iCanScript/Editor/Core/DisplayControllers/iCS_LibraryController.cs
@@ -336,8 +336,6 @@ public class iCS_LibraryController : DSTreeViewDataSource {
         if(iCS_Strings.IsEmpty(upperSearchStr)) return true;
         if(desc.DisplayName.ToUpper().IndexOf(upperSearchStr) != -1) return true;
         if(desc.ParentTypeInfo.DisplayName.ToUpper().IndexOf(upperSearchStr) != -1) return true;
-        if(!iCS_Strings.IsEmpty(desc.ParentTypeInfo.Library) && desc.Library.ToUpper().IndexOf(upperSearchStr) != -1) return true;
-        if(!iCS_Strings.IsEmpty(desc.ParentTypeInfo.Company) && desc.Company.ToUpper().IndexOf(upperSearchStr) != -1) return true;
         return false;
     }
     // ---------------------------------------------------------------------------------
