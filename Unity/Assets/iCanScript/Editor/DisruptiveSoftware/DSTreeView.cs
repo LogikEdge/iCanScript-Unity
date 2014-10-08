8df7cb4c83aaa0d35bb6b7d38e7cfec22a1914b8
Michel Launier <michel.launier@videotron.ca>
2014-10-08 12:34:21 -0400

    Remove code obseleted by the help embeded in the library database.

--- Unity/Assets/iCanScript/Editor/DisruptiveSoftware/DSTreeView.cs
+++ Unity/Assets/iCanScript/Editor/DisruptiveSoftware/DSTreeView.cs
@@ -197,9 +197,6 @@ public class DSTreeView : DSView {
     void ProcessEvents(Rect frameArea) {
      	Vector2 mousePosition= Event.current.mousePosition;
 		switch(Event.current.type) {
-            case EventType.ScrollWheel: {
-                break;
-            }
             case EventType.MouseDown: {
                 foreach(var keyValue in myRowInfo) {
                     Rect area= keyValue.Value;
@@ -208,24 +205,10 @@ public class DSTreeView : DSView {
                         var areaInScreenPoint= GUIUtility.GUIToScreenPoint(new Vector2(area.x, area.y));
                         var areaInScreenPosition= new Rect(areaInScreenPoint.x, areaInScreenPoint.y, area.width, area.height);
                         myDataSource.MouseDownOn(keyValue.Key, mouseInScreenPoint, areaInScreenPosition);						
-//                        Event.current.Use();
+                        // Don't consume the event to allow for aditional processing by the data controller.
+                        // Event.current.Use();
                         return;
                     }
-                }
-				break;
-			}
-            case EventType.MouseUp: {
-				break;
-			}
-            case EventType.MouseMove: {
-				//TODO: Can be removed once tips are cached in memberInfo.
-                foreach(var keyValue in myRowInfo) {
-				   Rect area= keyValue.Value;
-				   if(area.y < mousePosition.y && area.yMax > mousePosition.y) {
-					   myDataSource.MouseMove(keyValue.Key);	
-                       //Do not do an Event.current.Use(), or we may steal mouse moves from others.						
-					   return;
-					}
                 }
 				break;
 			}
