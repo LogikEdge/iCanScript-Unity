f9738ddcff9cd4d92bee7d6ea2dc4bc158392aaa
Michel Launier <michel.launier@videotron.ca>
2014-10-23 15:21:33 -0400

    Commenting error/warning display subsystem.

--- Unity/Assets/iCanScript/Editor/Core/Editors/VisualEditor/iCS_VisualEditor_ErrorsAndWarnings.cs
+++ Unity/Assets/iCanScript/Editor/Core/Editors/VisualEditor/iCS_VisualEditor_ErrorsAndWarnings.cs
@@ -19,29 +19,29 @@ public partial class iCS_VisualEditor {
     // Errors/Warning display functionality
 	// -----------------------------------------------------------------------
     void DisplayErrorsAndWarnings() {
-        // Nothing to display if no error/warning exists.
+        // -- Nothing to display if no error/warning exists --
         if(!iCS_ErrorController.IsErrorOrWarning) return;
-        // Update the repaint timer
+        // -- Update the repaint timer --
         UpdateErrorRepaintTimer();
-        // Show scene errors/warnings
+        // -- Show scene errors/warnings --
         DisplaySceneErrorsAndWarnings();
-        // Show errors/warnings on the nodes of our visual script
+        // -- Show errors/warnings on the nodes of our visual script --
         DisplayVisualScriptErrorsAndWarnings();
     }
     
 	// -----------------------------------------------------------------------
 	void DisplaySceneErrorsAndWarnings() {
-		// Nothing to show if no errors/warnings detected.
+		// -- Nothing to show if no errors/warnings detected --
         if(!iCS_ErrorController.IsErrorOrWarning) return;
 		
-        // Display scene error/warning icon
+        // -- Display scene error/warning icon --
 		var nbOfErrors  = iCS_ErrorController.NumberOfErrors;
         var nbOfWarnings= iCS_ErrorController.NumberOfWarnings;
         var r= GetSceneErrorWarningIconRect();
         var icon= nbOfErrors != 0 ? iCS_ErrorController.ErrorIcon : iCS_ErrorController.WarningIcon;
         DisplayErrorOrWarningIconWithAlpha(r, icon);
         
-        // Initiate the display of the scene error/warning details when mouse is over the icon
+        // -- Initiate the display of the scene error/warning details when mouse is over the icon --
 		if(r.Contains(WindowMousePosition)) {
 			showErrorDetails= true;
 			if(showErrorDetailTimer == null) {
@@ -53,9 +53,9 @@ public partial class iCS_VisualEditor {
 			}
 		}
 
-        // Display scene errors/warnings
+        // -- Display scene errors/warnings --
 		if(showErrorDetails) {
-			// Remove help viewport.
+			// -- Remove help viewport --
 			IsHelpEnabled= false;
 			
             var nbOfMessages= Mathf.Min(10, nbOfErrors+nbOfWarnings);
@@ -69,34 +69,34 @@ public partial class iCS_VisualEditor {
 
 	// -----------------------------------------------------------------------
     void DisplayVisualScriptErrorsAndWarnings() {
-        // Get errors/warnings for this visual script.
+        // -- Get errors/warnings for this visual script --
         var errors  = iCS_ErrorController.GetErrorsFor(VisualScript);
         var warnings= iCS_ErrorController.GetWarningsFor(VisualScript);
 
-        // Filter out invalid objects
+        // -- Filter out invalid objects --
         errors  = P.filter(e=> IStorage.IsValid(e.ObjectId), errors);
         warnings= P.filter(w=> IStorage.IsValid(w.ObjectId), warnings);
 
-        // Determine wich objects have errors or warnings.
+        // -- Determine wich objects have errors or warnings --
         var objectIds= P.append(P.map(e=> e.ObjectId, errors), P.map(w=> w.ObjectId, warnings));
         objectIds= P.removeDuplicates(objectIds);
 
-        // Display the errors/warnings on the objects in the graph.
+        // -- Display the errors/warnings on the objects in the graph --
         foreach(var id in objectIds) {
-            // Determine if node is visible
+            // -- Determine if node is visible --
             var node= IStorage[id];
             if(!DisplayRoot.IsParentOf(node)) continue;
             var pos= node.GlobalPosition;
             if(!VisibleGraphRect.Contains(pos)) continue;
-            // Determine errors/warnings for this particular node
+            // -- Determine errors/warnings for this particular node --
             var nodeErrors  = P.filter(e=> e.ObjectId == id, errors);
             var nodeWarnings= P.filter(w=> w.ObjectId == id, warnings);
-            // Display the appropriate error/warning icon
+            // -- Display the appropriate error/warning icon --
             var r= Math3D.BuildRectCenteredAt(pos, 32f, 32f);
             r= myGraphics.TranslateAndScale(r);
             var icon= nodeErrors.Count != 0 ? iCS_ErrorController.ErrorIcon : iCS_ErrorController.WarningIcon;
             DisplayErrorOrWarningIconWithAlpha(r, icon);
-            // Display error/warning details
+            // -- Display error/warning details --
             if(r.Contains(WindowMousePosition)) {
                 var nbOfMessages= Mathf.Min(5, nodeErrors.Count + nodeWarnings.Count);
                 var detailRect= DetermineErrorDetailRect(r, nbOfMessages);
@@ -118,7 +118,7 @@ public partial class iCS_VisualEditor {
     }
 	// -----------------------------------------------------------------------
     void DisplayErrorAndWarningDetails(Rect r, List<iCS_ErrorController.ErrorWarning> errors, List<iCS_ErrorController.ErrorWarning> warnings) {
-        // Draw background box
+        // -- Draw background box --
         var savedColor= GUI.color;
         var outlineRect= new Rect(r.x-2, r.y-2, r.width+4, r.height+4);
         GUI.color= errors.Count != 0 ? Color.red : Color.yellow;
@@ -127,11 +127,11 @@ public partial class iCS_VisualEditor {
 		GUI.Box(r,"");
 		GUI.color= savedColor;
 
-        // Define error/warning detail style.
+        // -- Define error/warning detail style --
 		GUIStyle style= EditorStyles.whiteLabel;
 		style.richText= true;
         
-        // Show Error first than Warnings.
+        // -- Show Error first than Warnings --
 		float y= 0;
 		GUI.BeginScrollView(r, Vector2.zero, new Rect(0,0,r.width,r.height));
         var content= new GUIContent("", iCS_ErrorController.SmallErrorIcon);
@@ -143,7 +143,7 @@ public partial class iCS_VisualEditor {
         content.image= iCS_ErrorController.SmallWarningIcon;
 		foreach(var w in warnings) {
 			content.text= w.Message;
-//			GUI.Label(new Rect(0, y, r.width, r.height), content, style);
+			GUI.Label(new Rect(0, y, r.width, r.height), content, style);
 			y+= 16;
 		}
 		GUI.EndScrollView();        
