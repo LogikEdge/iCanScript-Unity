c243064ed3ffeeb6c62fb84d5049b720ffa60d0e
Michel Launier <michel.launier@videotron.ca>
2014-12-08 16:59:23 -0500

    Improve upgrade experience

---
+++ Unity/Assets/iCanScript/Editor/0-Upgrade/iCS_UpgradeController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Collections;
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Validate & Perform upgrade.
+[InitializeOnLoad]
+public static class iCS_UpgradeController {
+    // ======================================================================
+    // Initialization
+    // ----------------------------------------------------------------------
+    static iCS_UpgradeController()    {
+		string kEditorCommunityDLL= "Assets/iCanScript/Editor/iCanScriptEditorCommunity.dll";
+		string kEditorProDLL      = Application.dataPath+"/iCanScript/Editor/iCanScriptEditorPro.dll";
+		if(File.Exists(kEditorProDLL)) {
+			AssetDatabase.DeleteAsset(kEditorCommunityDLL);			
+		}
+    }
+    public static void Start()      {}
+    public static void Shutdown() {
+    }
+}
--- Unity/Assets/iCanScript/Engine/iCS_BuildInfo.cs
+++ Unity/Assets/iCanScript/Engine/iCS_BuildInfo.cs
@@ -1,3 +1,3 @@
 public static class iCS_BuildInfo {
-public const string kBuildDateStr= "14/12/04 17:04:51";
+public const string kBuildDateStr= "14/12/08 16:52:28";
 }
