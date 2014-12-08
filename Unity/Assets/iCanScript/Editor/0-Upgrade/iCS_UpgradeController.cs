ede87bf3911f6a7f20f38f907b646ed9ff1d3f5f
Michel Launier <michel.launier@videotron.ca>
2014-12-08 17:09:09 -0500

    Prepare to add upgrade cleanup menu.

--- Unity/Assets/iCanScript/Editor/0-Upgrade/iCS_UpgradeController.cs
+++ Unity/Assets/iCanScript/Editor/0-Upgrade/iCS_UpgradeController.cs
@@ -11,13 +11,20 @@ public static class iCS_UpgradeController {
     // Initialization
     // ----------------------------------------------------------------------
     static iCS_UpgradeController()    {
-		string kEditorCommunityDLL= "Assets/iCanScript/Editor/iCanScriptEditorCommunity.dll";
-		string kEditorProDLL      = Application.dataPath+"/iCanScript/Editor/iCanScriptEditorPro.dll";
-		if(File.Exists(kEditorProDLL)) {
-			AssetDatabase.DeleteAsset(kEditorCommunityDLL);			
-		}
+		CleanupAfterImport();
     }
     public static void Start()      {}
     public static void Shutdown() {
     }
+
+    // ======================================================================
+    // Code cleanup
+    // ----------------------------------------------------------------------
+	pubic static void CleanupAfterImport() {
+		string kEditorCommunityDLL= "Assets/iCanScript/Editor/iCanScriptEditorCommunity.dll";
+		string kEditorProDLL      = Application.dataPath+"/iCanScript/Editor/iCanScriptEditorPro.dll";
+		if(File.Exists(kEditorProDLL)) {
+			AssetDatabase.DeleteAsset(kEditorCommunityDLL);			
+		}		
+	}
 }
