da53270086962ed7022c710cf5b78d3b27f5dd9c
Michel Launier <michel.launier@icloud.com>
2015-02-18 15:48:51 -0500

    Restart the package to it base definition (without iteration).

--- Unity/Assets/iCanScript/Engine/ExecutionService/iCS_Package.cs
+++ Unity/Assets/iCanScript/Engine/ExecutionService/iCS_Package.cs
@@ -4,31 +4,8 @@ using Subspace;
 
 public class iCS_Package : iCS_ParallelDispatcher {
     // ======================================================================
-    // Fields
-    // ----------------------------------------------------------------------
-    public int myIteration= 1;
-    
-    // ======================================================================
     // Creation/Destruction
     // ----------------------------------------------------------------------
     public iCS_Package(string name, SSObject parent, int priority, int nbOfParameters= 0, int nbOfEnables= 0)
     : base(name, parent, priority, nbOfParameters, nbOfEnables) {}
-
-    // ======================================================================
-    // Iteration
-    // ----------------------------------------------------------------------
-    protected override void DoEvaluate() {
-        if(myIteration > 1 && (myParent is iCS_Message) == true) {
-//            var packageContext= Context.Clone();
-//            packageContext.RunId+= 1000;
-//            Context= packageContext;
-            Context.RunId+= myIteration-1;
-            for(int i= 1; i < myIteration; ++i) {
-                base.DoEvaluate();
-                Context.RunId-= 1;
-            }
-//            Context= null;
-        }
-        base.DoEvaluate();
-    }
 }
