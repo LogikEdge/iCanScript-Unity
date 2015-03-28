36c225433f42b12339a134f6da3a36c4df1eacfa
Michel Launier <michel.launier@icloud.com>
2015-03-28 15:53:57 -0400

    Remove constructors and operation from Property Editor.

--- Unity/Assets/iCanScript/Editor/Core/DisplayControllers/iCS_PropertyController.cs
+++ Unity/Assets/iCanScript/Editor/Core/DisplayControllers/iCS_PropertyController.cs
@@ -10,23 +10,15 @@ public class iCS_PropertyController {
     // ---------------------------------------------------------------------------------
     iCS_EditorObject                myTarget             = null;
     iCS_IStorage                    myStorage            = null;
-    iCS_ConstructorInfo[]           myConstructors       = null;
-    int                             myConstructorIdx     = -1;
-    float                           myMaxConstructorWidth= 0f;
 
     DSTitleView                     myMainView            = null;
     DSVerticalLayoutView            myLayoutView          = null;
-    DSCellView                      myConstructorView     = null;
 	iCS_ClassVariablesController	myVariablesController = null;
-	iCS_ClassOperationsController	myOperationsController= null;
 
     // =================================================================================
     // Constant GUI Content
     // ---------------------------------------------------------------------------------
-    GUIContent  ConstructorTitle= new GUIContent("Instance");
     GUIContent  VariableTitle   = new GUIContent("Properties");
-    GUIContent  MethodTitle     = new GUIContent("Operations");
-    Vector2     ConstructorTitleSize;
     Vector2     LabelSize;
     float       LabelHeight;
     
@@ -52,50 +44,16 @@ public class iCS_PropertyController {
         myStorage= storage;
         if(!IsValid) return;
         
-        InitLayoutInfo();
-        InitConstructorInfo();
         BuildViews();
     }
     
     // ---------------------------------------------------------------------------------
-    void InitLayoutInfo() {
-        // Compute content size.
-        LabelSize           = EditorStyles.label.CalcSize(ConstructorTitle); 
-        LabelHeight         = 4f+LabelSize.y;
-        ConstructorTitleSize= EditorStyles.boldLabel.CalcSize(ConstructorTitle);        
-    }
-    
-    // ---------------------------------------------------------------------------------
-    void InitConstructorInfo() {
-        // Get and sort all constructors for the given class.
-        myConstructors= iCS_LibraryDatabase.GetConstructors(myTarget.RuntimeType);
-    	Array.Sort(myConstructors, (x,y)=> x.FunctionSignatureNoThis.CompareTo(y.FunctionSignatureNoThis));        
-
-        // Determine which constrcutor is currently used for this instance.
-        myConstructorIdx= -1;
-        for(int i= 0; i < myConstructors.Length; ++i) {
-            iCS_EditorObject existing= myStorage.InstanceWizardGetConstructor(myTarget);
-            if(existing != null && myConstructors[i].Method == existing.GetMethodBase(myStorage.EditorObjects)) {
-                myConstructorIdx= i;
-            }
-            var constructorSize= EditorStyles.boldLabel.CalcSize(new GUIContent(myConstructors[i].FunctionSignatureNoThisNoOutput));
-            if(constructorSize.x+12f > myMaxConstructorWidth) {
-                myMaxConstructorWidth= constructorSize.x+12f;
-            }
-        }        
-    }
-    
-    // ---------------------------------------------------------------------------------
     void BuildViews() {    
-        myConstructorView     = new DSCellView(new RectOffset(kSpacer,kSpacer,kSpacer,kSpacer), false, DrawConstructorCell, GetConstrcutorContentSize);
 		myVariablesController = new iCS_ClassVariablesController(myTarget.RuntimeType, myStorage, VariableTitle, myTarget);
-		myOperationsController= new iCS_ClassOperationsController(myTarget.RuntimeType, myStorage, MethodTitle, myTarget);
 
 		// Build class wizard layout view.
 		myLayoutView= new DSVerticalLayoutView(new RectOffset(0,0,0,0), false);
-		myLayoutView.AddSubview(myConstructorView, new RectOffset(0, 0, 0, 0));
 		myLayoutView.AddSubview(myVariablesController.View, new RectOffset(0, 0, 0, kSpacer), DSView.AnchorEnum.Center);
-		myLayoutView.AddSubview(myOperationsController.View, new RectOffset(0, 0, 0, 0), DSView.AnchorEnum.Center);
 		
 		// Build class wizard title view.
         string instanceTitle= myTarget.DisplayName;
@@ -104,35 +62,4 @@ public class iCS_PropertyController {
 		myMainView.SetSubview(myLayoutView);
     }
 
-    // =================================================================================
-    // Constructor selection view
-    // ---------------------------------------------------------------------------------
-    Vector2 GetConstrcutorContentSize(DSView view, Rect displayArea) {
-        return new Vector2(ConstructorTitleSize.x+2f*kSpacer+myMaxConstructorWidth, LabelHeight);
-    }
-    void DrawConstructorCell(DSView view, Rect displayArea) {
-        float y= displayArea.y;
-        GUI.Label(new Rect(displayArea.x, y, ConstructorTitleSize.x, ConstructorTitleSize.y), ConstructorTitle, EditorStyles.boldLabel);
-        float x= displayArea.x+ConstructorTitleSize.x+2f*kSpacer;
-        // Fill-in available constructors.
-        string[] instanceOptions= new string[1+myConstructors.Length];
-        instanceOptions[0]= "Use input port (this)";
-        for(int i= 0; i < myConstructors.Length; ++i) {
-            instanceOptions[i+1]= myConstructors[i].FunctionSignatureNoThisNoOutput;
-        }
-        float maxWidth= displayArea.width-(ConstructorTitleSize.x+2f*kSpacer);
-        float width= Mathf.Min(maxWidth, myMaxConstructorWidth);
-        int instanceIdx= myConstructorIdx+1;
-        int newIdx= EditorGUI.Popup(new Rect(x, y, width, LabelHeight), instanceIdx, instanceOptions, EditorStyles.toolbarPopup);
-        if(GUI.changed) {
-            myConstructorIdx= -1;
-            if(instanceIdx != 0) {
-                myStorage.InstanceWizardDestroyConstructor(myTarget);
-            }
-            if(newIdx != 0) {
-                myConstructorIdx= newIdx-1;
-                myStorage.InstanceWizardCreateConstructor(myTarget, myConstructors[newIdx-1]);
-            }
-        }
-    }
 }
