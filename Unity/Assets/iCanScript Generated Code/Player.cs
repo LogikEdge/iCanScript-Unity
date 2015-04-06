b6472742f161b1f451e6f10fd739d596594aa8d3
Michel Launier <michel.launier@icloud.com>
2015-04-06 10:59:04 -0400

    Updated SpaceShooter Chapter 2

--- Unity/Assets/iCanScript Generated Code/Player.cs
+++ Unity/Assets/iCanScript Generated Code/Player.cs
@@ -8,7 +8,11 @@ namespace iCanScript.Engine.GeneratedCode {
         // PUBLIC FIELDS
         // -------------------------------------------------------------------------
         [iCS_InOutPort]
-        public  float speed= 10f;
+        public  iCS_PulseGenerator pulseGenerator=  new iCS_PulseGenerator();
+        [iCS_InOutPort]
+        public  Object p= Bolt (UnityEngine.GameObject);
+        [iCS_InOutPort]
+        public  Transform transformInstance= Shot Spawn (UnityEngine.Transform);
         [iCS_InOutPort]
         public  float left= -6f;
         [iCS_InOutPort]
@@ -23,7 +27,7 @@ namespace iCanScript.Engine.GeneratedCode {
         // =========================================================================
         // PRIVATE FIELDS
         // -------------------------------------------------------------------------
-        private  Vector2 p_analog1= default(Vector2);
+        private  Vector2 p_scaledAnalog1= default(Vector2);
 
 
         // =========================================================================
@@ -32,7 +36,19 @@ namespace iCanScript.Engine.GeneratedCode {
 
         [iCS_Function]
         public  void Update() {
-            p_analog1= iCS_Math.Scale2Vector(Time.deltaTime, speed, iCS_FromTo.ToVector(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+            var theAudioSource= GetComponent<AudioSource>();
+            bool theB3;
+            bool theB2;
+            bool theB1;
+            Vector2 theAnalog1;
+            p_scaledAnalog1= iCS_GameController.GameController(out theAnalog1, out theB1, out theB2, out theB3, 10f);
+            if(theB1) {
+                var thePulse= pulseGenerator.GeneratePulse(0.25f, true, true);
+                if(thePulse) {
+                    Object.Instantiate(p, transformInstance.position, transformInstance.rotation);
+                    theAudioSource.Play();
+                }
+            }
         }
 
         [iCS_Function]
@@ -40,7 +56,7 @@ namespace iCanScript.Engine.GeneratedCode {
             var theRigidbody= GetComponent<Rigidbody>();
             var thePosition= theRigidbody.position;
             var theVelocity= theRigidbody.velocity;
-            theRigidbody.velocity= iCS_FromTo.ToVector(p_analog1.x, 0f, p_analog1.y);
+            theRigidbody.velocity= iCS_FromTo.ToVector(p_scaledAnalog1.x, 0f, p_scaledAnalog1.y);
             theRigidbody.position= iCS_FromTo.ToVector(Mathf.Clamp(thePosition.x, left, right), thePosition.y, Mathf.Clamp(thePosition.z, bottom, top));
             theRigidbody.rotation= Quaternion.Euler(0f, 0f, iCS_Math.Mul(theVelocity.x, tiltFactor));
         }
