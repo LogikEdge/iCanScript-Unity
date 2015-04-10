f0de16a9b47a0c239dd6b6fc9a6a30c2f2c3e08a
Michel Launier <michel.launier@icloud.com>
2015-04-10 10:56:34 -0400

    Again, removal of iCS1 runtime nodes.

--- Unity/Assets/iCanScript/Engine/ExecutionService/iCS_Package.cs
+++ Unity/Assets/iCanScript/Engine/ExecutionService/iCS_Package.cs
@@ -1,11 +1,1 @@
-using UnityEngine;
-using System;
-using Subspace;
-
-public class iCS_Package : iCS_ParallelDispatcher {
-    // ======================================================================
-    // Creation/Destruction
-    // ----------------------------------------------------------------------
-    public iCS_Package(string name, SSObject parent, int priority, int nbOfParameters= 0, int nbOfEnables= 0)
-    : base(name, parent, priority, nbOfParameters, nbOfEnables) {}
-}
+public class iCS_Package {}
