09580a5067ab1e1445407bb2c26c09e5ec8de4f9
Michel Launier <michel.launier@icloud.com>
2015-04-12 09:56:55 -0400

    Remove the Unity inspector editor. since it is superseeded by the in-graph specific node/port editors.

--- Unity/Assets/iCanScript/Editor/EditorWindows/iCS_LibraryInspector.cs
+++
@@ -1,5 +0,0 @@
-using UnityEngine;
-using UnityEditor;
-
-[CustomEditor (typeof (iCS_Library))]
-public sealed class iCS_LibraryInspector : iCS_Inspector {}
--- Unity/Assets/iCanScript/Editor/EditorWindows/iCS_VisualScriptInspector.cs
+++
@@ -1,5 +0,0 @@
-using UnityEngine;
-using UnityEditor;
-
-[CustomEditor (typeof (iCS_VisualScript))]
-public sealed class iCS_VisualScriptInspector : iCS_Inspector {}
