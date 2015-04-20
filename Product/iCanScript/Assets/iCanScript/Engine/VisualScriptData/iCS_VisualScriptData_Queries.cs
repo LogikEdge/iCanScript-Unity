6353c22e1cc6f3776b9dfe27e26715978cf62797
Michel Launier <michel.launier@icloud.com>
2015-04-20 10:37:35 -0400

    Remove more dead code.

--- Product/iCanScript/Assets/iCanScript/Engine/VisualScriptData/iCS_VisualScriptData_Queries.cs
+++ Product/iCanScript/Assets/iCanScript/Engine/VisualScriptData/iCS_VisualScriptData_Queries.cs
@@ -13,25 +13,6 @@ public partial class iCS_VisualScriptData : iCS_IVisualScriptData {
         int id= obj.InstanceId;
         return id >= 0 && id < vsd.EngineObjects.Count;
     }
-    // -----------------------------------------------------------------------
-    // Returns 'true' if the given EngineObject passed the test.
-    public static bool Test(iCS_EngineObject o, Func<iCS_EngineObject, bool> cond, iCS_IVisualScriptData vsd) {
-        return IsValid(o, vsd) && cond(o);
-    }
-    // ----------------------------------------------------------------------
-    // Returns 'true' if the given engine object is a public varaiable.
-    public static bool IsPublicVariable(iCS_IVisualScriptData vsd, iCS_EngineObject obj) {
-        if(obj == null) return false;
-        if(obj.ParentId != 0) return false;
-        return obj.IsConstructor && vsd.EngineObjects[0].IsBehaviour;
-    }
-    // ----------------------------------------------------------------------
-    // Returns 'true' if the given engine object is a user function.
-    public static bool IsPublicFunction(iCS_IVisualScriptData vsd, iCS_EngineObject obj) {
-        if(obj == null) return false;
-        if(obj.ParentId != 0) return false;
-        return obj.IsPackage && vsd.EngineObjects[0].IsBehaviour;
-    }
     
     // =======================================================================
     // General Iterations
@@ -44,11 +25,5 @@ public partial class iCS_VisualScriptData : iCS_IVisualScriptData {
     // Invokes the given action if the given condition has passed the test.
     public static void FilterWith(Func<iCS_EngineObject,bool> cond, Action<iCS_EngineObject> action, iCS_IVisualScriptData vsd) {
         ForEach( o=> { if(cond(o)) action(o); }, vsd);
-    }
-    // -----------------------------------------------------------------------
-    // Finds the first engine object that passes the test
-    public static iCS_EngineObject Find(iCS_IVisualScriptData vsd, Func<iCS_EngineObject,bool> cond) {
-        foreach(var o in vsd.EngineObjects) { if(Test(o, cond, vsd)) return o; }
-        return null;
     }
 }
