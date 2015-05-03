691b2f9ac6b59c01d329a339d19e85a92369512f
Michel Launier <michel.launier@icloud.com>
2015-05-03 10:13:00 -0400

    Move DSView into namespace iCanScript.Editor

--- Product/iCanScript/Assets/iCanScript/Editor/Core/DisplayControllers/iCS_ClassOperationsController.cs
+++ Product/iCanScript/Assets/iCanScript/Editor/Core/DisplayControllers/iCS_ClassOperationsController.cs
@@ -4,108 +4,112 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-public class iCS_ClassOperationsController : DSTableViewDataSource {
-    // =================================================================================
-    // Constants
-    // ---------------------------------------------------------------------------------
-    const int     kSpacer           = 8;
-    const int     kMarginSize       = 10;
-	const string  kDefaultTitle     = "Operations";
-	const string  kOperationColumnId= "Operation";
+namespace iCanScript.Editor {
+    
+    public class iCS_ClassOperationsController : DSTableViewDataSource {
+        // =================================================================================
+        // Constants
+        // ---------------------------------------------------------------------------------
+        const int     kSpacer           = 8;
+        const int     kMarginSize       = 10;
+    	const string  kDefaultTitle     = "Operations";
+    	const string  kOperationColumnId= "Operation";
 
-    // =================================================================================
-    // Fields
-    // ---------------------------------------------------------------------------------
-	Type				myClassType      = null;
-	iCS_EditorObject	myTarget         = null;
-	iCS_IStorage		myStorage        = null;
-    GUIContent    		myTitle          = null;
-    DSTableView			myTableView      = null;
-	iCS_MethodInfo[]    myMethods        = null;
-	bool[]              myIsMethodPresent= null;
+        // =================================================================================
+        // Fields
+        // ---------------------------------------------------------------------------------
+    	Type				myClassType      = null;
+    	iCS_EditorObject	myTarget         = null;
+    	iCS_IStorage		myStorage        = null;
+        GUIContent    		myTitle          = null;
+        DSTableView			myTableView      = null;
+    	iCS_MethodInfo[]    myMethods        = null;
+    	bool[]              myIsMethodPresent= null;
     
-    // =================================================================================
-    // Properties
-    // ---------------------------------------------------------------------------------
-    public DSView View { get { return myTableView; }}
+        // =================================================================================
+        // Properties
+        // ---------------------------------------------------------------------------------
+        public DSView View { get { return myTableView; }}
     
-    // =================================================================================
-    // Initialization
-    // ---------------------------------------------------------------------------------
-	public iCS_ClassOperationsController() {}
-    public iCS_ClassOperationsController(Type classType, iCS_IStorage storage, GUIContent title= null, iCS_EditorObject target= null) {
-		OnActivate(classType, storage, title, target);
-	}
-	public void OnActivate(Type classType, iCS_IStorage storage, GUIContent title= null, iCS_EditorObject target= null) {
-		// Configuration parameters.
-		myClassType= classType;
-		myStorage= storage;
-		myTitle= title ?? new GUIContent(kDefaultTitle);
-		myTarget= target;
+        // =================================================================================
+        // Initialization
+        // ---------------------------------------------------------------------------------
+    	public iCS_ClassOperationsController() {}
+        public iCS_ClassOperationsController(Type classType, iCS_IStorage storage, GUIContent title= null, iCS_EditorObject target= null) {
+    		OnActivate(classType, storage, title, target);
+    	}
+    	public void OnActivate(Type classType, iCS_IStorage storage, GUIContent title= null, iCS_EditorObject target= null) {
+    		// Configuration parameters.
+    		myClassType= classType;
+    		myStorage= storage;
+    		myTitle= title ?? new GUIContent(kDefaultTitle);
+    		myTarget= target;
 		
-		// Extract fields & properties from class descriptor.
-        myMethods= iCS_LibraryDatabase.GetMethods(myClassType);
-    	Array.Sort(myMethods, (x,y)=> x.FunctionSignatureNoThis.CompareTo(y.FunctionSignatureNoThis));
+    		// Extract fields & properties from class descriptor.
+            myMethods= iCS_LibraryDatabase.GetMethods(myClassType);
+        	Array.Sort(myMethods, (x,y)=> x.FunctionSignatureNoThis.CompareTo(y.FunctionSignatureNoThis));
 
-        // Build method presence on the given target.
-    	myIsMethodPresent= new bool[myMethods.Length];
-        if(myTarget != null && myStorage != null) {
-            for(int i= 0; i < myMethods.Length; ++i) {
-                myIsMethodPresent[i]= myStorage.InstanceWizardFindFunction(myTarget, myMethods[i]) != null;
-            }            
-        }
-
-		// Build view
-        myTableView= new DSTableView(new RectOffset(0,0,0,0), true, myTitle, DSView.AnchorEnum.Center, true, true);
-        myTableView.DataSource= this;
-		DSTableColumn operationColumn= new DSTableColumn(kOperationColumnId, new RectOffset(0,0,0,0), null, DSView.AnchorEnum.CenterLeft);
-		myTableView.AddColumn(operationColumn);
-    }
-
-    // =================================================================================
-    // TableViewDataSource
-    // ---------------------------------------------------------------------------------
-    public int NumberOfRowsInTableView(DSTableView tableView) {
-		return myClassType != null ? myMethods.Length : 0;
-    }
-    // ---------------------------------------------------------------------------------
-    public Vector2 LayoutSizeForObjectInTableView(DSTableView tableView, DSTableColumn tableColumn, int row) {
-		if(myClassType == null) return Vector2.zero;
-        var signatureSize= EditorStyles.boldLabel.CalcSize(new GUIContent(myMethods[row].FunctionSignatureNoThis));
-		signatureSize.x+= 12f;
-		return signatureSize;
-    }
-    // ---------------------------------------------------------------------------------
-    public void DisplayObjectInTableView(DSTableView tableView, DSTableColumn tableColumn, int row, Rect position) {
-		if(myClassType == null) return;
-        GUIStyle style= GUI.skin.button;
-        var alignment= style.alignment;
-        var fontStyle= style.fontStyle;
-        var textColor= style.normal.textColor;        
-        var background= style.normal.background;
-        style.alignment= TextAnchor.MiddleLeft;
-        if(myIsMethodPresent[row]) {
-            style.normal.textColor= Color.white;
-            style.fontStyle= FontStyle.Bold;
-            style.normal.background= style.active.background;            
-        } else {
-            style.fontStyle= FontStyle.Italic;
-        }
-		position.width= tableColumn.DataSize.x;
-        if(GUI.Button(position, myMethods[row].FunctionSignatureNoThis) && myTarget != null && myStorage != null) {
-            myIsMethodPresent[row]^= true;
-            if(myIsMethodPresent[row]) {
-                iCS_UserCommands.CreateInstanceWizardElement(myTarget, myMethods[row]);
-            } else {
-                iCS_UserCommands.DeleteInstanceWizardElement(myTarget, myMethods[row]);
+            // Build method presence on the given target.
+        	myIsMethodPresent= new bool[myMethods.Length];
+            if(myTarget != null && myStorage != null) {
+                for(int i= 0; i < myMethods.Length; ++i) {
+                    myIsMethodPresent[i]= myStorage.InstanceWizardFindFunction(myTarget, myMethods[i]) != null;
+                }            
             }
+
+    		// Build view
+            myTableView= new DSTableView(new RectOffset(0,0,0,0), true, myTitle, DSView.AnchorEnum.Center, true, true);
+            myTableView.DataSource= this;
+    		DSTableColumn operationColumn= new DSTableColumn(kOperationColumnId, new RectOffset(0,0,0,0), null, DSView.AnchorEnum.CenterLeft);
+    		myTableView.AddColumn(operationColumn);
         }
-        style.normal.textColor= textColor;
-        style.normal.background= background;
-        style.fontStyle= fontStyle;
-        style.alignment= alignment;
+
+        // =================================================================================
+        // TableViewDataSource
+        // ---------------------------------------------------------------------------------
+        public int NumberOfRowsInTableView(DSTableView tableView) {
+    		return myClassType != null ? myMethods.Length : 0;
+        }
+        // ---------------------------------------------------------------------------------
+        public Vector2 LayoutSizeForObjectInTableView(DSTableView tableView, DSTableColumn tableColumn, int row) {
+    		if(myClassType == null) return Vector2.zero;
+            var signatureSize= EditorStyles.boldLabel.CalcSize(new GUIContent(myMethods[row].FunctionSignatureNoThis));
+    		signatureSize.x+= 12f;
+    		return signatureSize;
+        }
+        // ---------------------------------------------------------------------------------
+        public void DisplayObjectInTableView(DSTableView tableView, DSTableColumn tableColumn, int row, Rect position) {
+    		if(myClassType == null) return;
+            GUIStyle style= GUI.skin.button;
+            var alignment= style.alignment;
+            var fontStyle= style.fontStyle;
+            var textColor= style.normal.textColor;        
+            var background= style.normal.background;
+            style.alignment= TextAnchor.MiddleLeft;
+            if(myIsMethodPresent[row]) {
+                style.normal.textColor= Color.white;
+                style.fontStyle= FontStyle.Bold;
+                style.normal.background= style.active.background;            
+            } else {
+                style.fontStyle= FontStyle.Italic;
+            }
+    		position.width= tableColumn.DataSize.x;
+            if(GUI.Button(position, myMethods[row].FunctionSignatureNoThis) && myTarget != null && myStorage != null) {
+                myIsMethodPresent[row]^= true;
+                if(myIsMethodPresent[row]) {
+                    iCS_UserCommands.CreateInstanceWizardElement(myTarget, myMethods[row]);
+                } else {
+                    iCS_UserCommands.DeleteInstanceWizardElement(myTarget, myMethods[row]);
+                }
+            }
+            style.normal.textColor= textColor;
+            style.normal.background= background;
+            style.fontStyle= fontStyle;
+            style.alignment= alignment;
+        }
+        // ---------------------------------------------------------------------------------
+    	public void OnMouseDown(DSTableView tableView, DSTableColumn tableColumn, int row) {}
+
     }
-    // ---------------------------------------------------------------------------------
-	public void OnMouseDown(DSTableView tableView, DSTableColumn tableColumn, int row) {}
 
 }
--- Product/iCanScript/Assets/iCanScript/Editor/DisruptiveSoftware/DSAccordionView.cs
+++ Product/iCanScript/Assets/iCanScript/Editor/DisruptiveSoftware/DSAccordionView.cs
@@ -2,144 +2,147 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class DSAccordionView : DSView {
-    // ======================================================================
-    // Fields
-    // ----------------------------------------------------------------------
-	DSVerticalLayoutView	myMainView         = null;
-	DSCellView				mySelectionView    = null;
-	GUIContent[]			mySelectionIds     = null;
-	DSView[]				mySubviews         = null;
-	int						mySelectionIdx     = 0;
-	int						mySelectionsPerLine= 1;
+namespace iCanScript.Editor {
+    public class DSAccordionView : DSView {
+        // ======================================================================
+        // Fields
+        // ----------------------------------------------------------------------
+    	DSVerticalLayoutView	myMainView         = null;
+    	DSCellView				mySelectionView    = null;
+    	GUIContent[]			mySelectionIds     = null;
+    	DSView[]				mySubviews         = null;
+    	int						mySelectionIdx     = 0;
+    	int						mySelectionsPerLine= 1;
 	
-    // =================================================================================
-    // Constants
-    // ---------------------------------------------------------------------------------
-    const int   kSpacer= 8;
+        // =================================================================================
+        // Constants
+        // ---------------------------------------------------------------------------------
+        const int   kSpacer= 8;
     
-    // ======================================================================
-    // Properties
-    // ----------------------------------------------------------------------
-	public DSView View { get { return myMainView; }}
-	public int SelectionsPerLine {
-		get { return mySelectionsPerLine; }
-		set { mySelectionsPerLine= value < 1 ? 1 : value; }
-	}
+        // ======================================================================
+        // Properties
+        // ----------------------------------------------------------------------
+    	public DSView View { get { return myMainView; }}
+    	public int SelectionsPerLine {
+    		get { return mySelectionsPerLine; }
+    		set { mySelectionsPerLine= value < 1 ? 1 : value; }
+    	}
 	
-    // ======================================================================
-    // Initialization
-    // ----------------------------------------------------------------------
-	public DSAccordionView(RectOffset margins, bool shouldDisplayFrame, int selectionsPerLine= 1) {
-        mySelectionsPerLine= selectionsPerLine;
-		mySelectionIds= new GUIContent[0];
-		mySubviews= new DSView[0];
+        // ======================================================================
+        // Initialization
+        // ----------------------------------------------------------------------
+    	public DSAccordionView(RectOffset margins, bool shouldDisplayFrame, int selectionsPerLine= 1) {
+            mySelectionsPerLine= selectionsPerLine;
+    		mySelectionIds= new GUIContent[0];
+    		mySubviews= new DSView[0];
 		
-		myMainView= new DSVerticalLayoutView(margins, shouldDisplayFrame);
-		mySelectionView= new DSCellView(new RectOffset(0,0,0,0), false, DisplaySelection, GetSelectionSize);
-		myMainView.AddSubview(mySelectionView, new RectOffset(0,0,kSpacer,0));
-	}
+    		myMainView= new DSVerticalLayoutView(margins, shouldDisplayFrame);
+    		mySelectionView= new DSCellView(new RectOffset(0,0,0,0), false, DisplaySelection, GetSelectionSize);
+    		myMainView.AddSubview(mySelectionView, new RectOffset(0,0,kSpacer,0));
+    	}
 
-	// ======================================================================
-	// DSView functionality implementation.
-	// ----------------------------------------------------------------------
-	public override void Display(Rect frameArea) { 
-		int previousSelectionIdx= mySelectionIdx;
-		myMainView.Display(frameArea);
-		if(mySelectionIdx != previousSelectionIdx) {
-			RemoveSwapableSubview(previousSelectionIdx);
-			AddSwapableSubview(mySelectionIdx);			
-		}
-	}
-	public override Vector2 GetSizeToDisplay(Rect frameArea) {
-		return myMainView.GetSizeToDisplay(frameArea);
-	}
-	public override AnchorEnum GetAnchor() {
-		return myMainView.GetAnchor();
-	}
-	public override void SetAnchor(AnchorEnum anchor) {
-		myMainView.SetAnchor(anchor);
-	}
+    	// ======================================================================
+    	// DSView functionality implementation.
+    	// ----------------------------------------------------------------------
+    	public override void Display(Rect frameArea) { 
+    		int previousSelectionIdx= mySelectionIdx;
+    		myMainView.Display(frameArea);
+    		if(mySelectionIdx != previousSelectionIdx) {
+    			RemoveSwapableSubview(previousSelectionIdx);
+    			AddSwapableSubview(mySelectionIdx);			
+    		}
+    	}
+    	public override Vector2 GetSizeToDisplay(Rect frameArea) {
+    		return myMainView.GetSizeToDisplay(frameArea);
+    	}
+    	public override AnchorEnum GetAnchor() {
+    		return myMainView.GetAnchor();
+    	}
+    	public override void SetAnchor(AnchorEnum anchor) {
+    		myMainView.SetAnchor(anchor);
+    	}
 
-	// ======================================================================
-	// Selection view implementation.
-	// ----------------------------------------------------------------------
-	Vector2 GetSelectionSize(DSCellView view, Rect displaySize) {
-        float width= 0;
-        float height= 0;
-		foreach(var selection in mySelectionIds) {
-			var sSize= GUI.skin.button.CalcSize(selection);
-			if(sSize.x > width) width= sSize.x;
-            if(sSize.y > height) height= sSize.y;
-		}			
-		return new Vector2((width+kSpacer)*mySelectionsPerLine, height*((mySelectionsPerLine+mySelectionsPerLine-1)/mySelectionsPerLine));
-	}
-	void DisplaySelection(DSCellView view, Rect position) {
-		if(mySelectionIds.Length < 1) return;
-		mySelectionIdx= GUI.SelectionGrid(position, mySelectionIdx, mySelectionIds, SelectionsPerLine);
-	}
+    	// ======================================================================
+    	// Selection view implementation.
+    	// ----------------------------------------------------------------------
+    	Vector2 GetSelectionSize(DSCellView view, Rect displaySize) {
+            float width= 0;
+            float height= 0;
+    		foreach(var selection in mySelectionIds) {
+    			var sSize= GUI.skin.button.CalcSize(selection);
+    			if(sSize.x > width) width= sSize.x;
+                if(sSize.y > height) height= sSize.y;
+    		}			
+    		return new Vector2((width+kSpacer)*mySelectionsPerLine, height*((mySelectionsPerLine+mySelectionsPerLine-1)/mySelectionsPerLine));
+    	}
+    	void DisplaySelection(DSCellView view, Rect position) {
+    		if(mySelectionIds.Length < 1) return;
+    		mySelectionIdx= GUI.SelectionGrid(position, mySelectionIdx, mySelectionIds, SelectionsPerLine);
+    	}
 	
-    // ======================================================================
-    // Subview management
-    // ----------------------------------------------------------------------
-    public void AddSubview(GUIContent selecionId, DSView subview) {
-		// Add to selection id array.
-		System.Array.Resize(ref mySelectionIds, mySelectionIds.Length+1);
-		mySelectionIds[mySelectionIds.Length-1]= selecionId;
-		// Add to subview array.
-		System.Array.Resize(ref mySubviews, mySubviews.Length+1);
-		mySubviews[mySubviews.Length-1]= subview;
-		// Prime first subview.
-		if(mySubviews.Length == 1) {
-			mySelectionIdx= 0;
-			AddSwapableSubview(mySelectionIdx);
-		}
+        // ======================================================================
+        // Subview management
+        // ----------------------------------------------------------------------
+        public void AddSubview(GUIContent selecionId, DSView subview) {
+    		// Add to selection id array.
+    		System.Array.Resize(ref mySelectionIds, mySelectionIds.Length+1);
+    		mySelectionIds[mySelectionIds.Length-1]= selecionId;
+    		// Add to subview array.
+    		System.Array.Resize(ref mySubviews, mySubviews.Length+1);
+    		mySubviews[mySubviews.Length-1]= subview;
+    		// Prime first subview.
+    		if(mySubviews.Length == 1) {
+    			mySelectionIdx= 0;
+    			AddSwapableSubview(mySelectionIdx);
+    		}
+        }
+        // ----------------------------------------------------------------------
+        public bool RemoveSubview(DSView subview) {
+    		bool found= false;
+    		int idx= 0;
+    		for(; idx < mySubviews.Length; ++idx) {
+    			if(mySubviews[idx] == subview) {
+    				found= true;
+    				break;
+    			}
+    		}
+    		if(!found) return false;
+    		// Recompute selected subview if it was removed.
+    		bool needNewSelection= false;
+    		if(idx == mySelectionIdx) {
+    			RemoveSwapableSubview(mySelectionIdx);
+    			needNewSelection= true;
+    		}
+    		// Cleanup internal information.
+    		for(int i= idx+1; i < mySelectionIds.Length; ++i) {
+    			mySelectionIds[i-1]= mySelectionIds[i];
+    		}
+    		System.Array.Resize(ref mySelectionIds, mySelectionIds.Length-1);
+    		for(int i= idx+1; i < mySubviews.Length; ++i) {
+    			mySubviews[i-1]= mySubviews[i];
+    		}
+    		System.Array.Resize(ref mySubviews, mySubviews.Length-1);
+    		// Rearrange selection.
+    		if(needNewSelection) {
+    			if(mySelectionIdx != 0) {
+    				--mySelectionIdx;
+    			}
+    			AddSwapableSubview(mySelectionIdx);
+    		}
+            return true;
+        }
+        // ----------------------------------------------------------------------
+    	void AddSwapableSubview(int idx) {
+    		if(idx < mySubviews.Length) {
+    			myMainView.AddSubview(mySubviews[idx], new RectOffset(0,0,kSpacer,0));
+    		}
+    	}
+        // ----------------------------------------------------------------------
+    	void RemoveSwapableSubview(int idx) {
+    		if(idx < mySubviews.Length) {
+    			myMainView.RemoveSubview(mySubviews[idx]);
+    		}
+    	}
     }
-    // ----------------------------------------------------------------------
-    public bool RemoveSubview(DSView subview) {
-		bool found= false;
-		int idx= 0;
-		for(; idx < mySubviews.Length; ++idx) {
-			if(mySubviews[idx] == subview) {
-				found= true;
-				break;
-			}
-		}
-		if(!found) return false;
-		// Recompute selected subview if it was removed.
-		bool needNewSelection= false;
-		if(idx == mySelectionIdx) {
-			RemoveSwapableSubview(mySelectionIdx);
-			needNewSelection= true;
-		}
-		// Cleanup internal information.
-		for(int i= idx+1; i < mySelectionIds.Length; ++i) {
-			mySelectionIds[i-1]= mySelectionIds[i];
-		}
-		System.Array.Resize(ref mySelectionIds, mySelectionIds.Length-1);
-		for(int i= idx+1; i < mySubviews.Length; ++i) {
-			mySubviews[i-1]= mySubviews[i];
-		}
-		System.Array.Resize(ref mySubviews, mySubviews.Length-1);
-		// Rearrange selection.
-		if(needNewSelection) {
-			if(mySelectionIdx != 0) {
-				--mySelectionIdx;
-			}
-			AddSwapableSubview(mySelectionIdx);
-		}
-        return true;
-    }
-    // ----------------------------------------------------------------------
-	void AddSwapableSubview(int idx) {
-		if(idx < mySubviews.Length) {
-			myMainView.AddSubview(mySubviews[idx], new RectOffset(0,0,kSpacer,0));
-		}
-	}
-    // ----------------------------------------------------------------------
-	void RemoveSwapableSubview(int idx) {
-		if(idx < mySubviews.Length) {
-			myMainView.RemoveSubview(mySubviews[idx]);
-		}
-	}
+    
 }
