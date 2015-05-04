bbd4663469748945d3b74db909542159c2fe0c08
Michel Launier <michel.launier@icloud.com>
2015-05-04 07:42:34 -0400

    Move more iCanScript code inside iCanScript namespaces.

--- Product/iCanScript/Assets/iCanScript/Editor/Web/iCS_WebUtils.cs
+++ Product/iCanScript/Assets/iCanScript/Editor/Web/iCS_WebUtils.cs
@@ -6,47 +6,51 @@ using System;
 using System.Collections;
 using P=Prelude;
 
-public static class iCS_WebUtils {
-    // ----------------------------------------------------------------------
-    // Performs a Web form request on the given url.
-    public static WWW WebRequest(string url, WWWForm form, float waitTime= 2f) {
-        WWW response= null;
-		try {
-			response= WaitForTransaction(new WWW(url, form), waitTime);			
-		}
-#if DEBUG
-		catch(System.Exception e) { Debug.LogWarning("iCanScript: Error accessing WWW: "+e.Message); }
-#else
-		catch(System.Exception) {}
-#endif
-		return response;
-    }
-    // ----------------------------------------------------------------------
-    // Performs a Web request on the given url.
-    public static WWW WebRequest(string url, float waitTime= 500f) {
-        WWW response= null;
-		try {
-			response= WaitForTransaction(new WWW(url), waitTime);			
-		}
-#if DEBUG
-		catch(System.Exception e) { Debug.LogWarning("iCanScript: Error accessing WWW: "+e.Message); }
-#else
-		catch(System.Exception) {}
-#endif
-		return response;
-    }
-    // ----------------------------------------------------------------------
-	// Wait for Web transaction to complete.
-    public static WWW WaitForTransaction(WWW transaction, float waitTime= 2f) {
-        var startTime= Time.realtimeSinceStartup;
-        while(!transaction.isDone) {
-            if((Time.realtimeSinceStartup-startTime) > waitTime) {
-#if DEBUG
-                Debug.LogWarning("iCanScript: Timeout waiting for URL: "+transaction.url);
-#endif
-                break;
-            }
+namespace iCanScript.Editor {
+    
+    public static class iCS_WebUtils {
+        // ----------------------------------------------------------------------
+        // Performs a Web form request on the given url.
+        public static WWW WebRequest(string url, WWWForm form, float waitTime= 2f) {
+            WWW response= null;
+    		try {
+    			response= WaitForTransaction(new WWW(url, form), waitTime);			
+    		}
+    #if DEBUG
+    		catch(System.Exception e) { Debug.LogWarning("iCanScript: Error accessing WWW: "+e.Message); }
+    #else
+    		catch(System.Exception) {}
+    #endif
+    		return response;
         }
-        return transaction;	
-	}
+        // ----------------------------------------------------------------------
+        // Performs a Web request on the given url.
+        public static WWW WebRequest(string url, float waitTime= 500f) {
+            WWW response= null;
+    		try {
+    			response= WaitForTransaction(new WWW(url), waitTime);			
+    		}
+    #if DEBUG
+    		catch(System.Exception e) { Debug.LogWarning("iCanScript: Error accessing WWW: "+e.Message); }
+    #else
+    		catch(System.Exception) {}
+    #endif
+    		return response;
+        }
+        // ----------------------------------------------------------------------
+    	// Wait for Web transaction to complete.
+        public static WWW WaitForTransaction(WWW transaction, float waitTime= 2f) {
+            var startTime= Time.realtimeSinceStartup;
+            while(!transaction.isDone) {
+                if((Time.realtimeSinceStartup-startTime) > waitTime) {
+    #if DEBUG
+                    Debug.LogWarning("iCanScript: Timeout waiting for URL: "+transaction.url);
+    #endif
+                    break;
+                }
+            }
+            return transaction;	
+    	}
+    }
+
 }
