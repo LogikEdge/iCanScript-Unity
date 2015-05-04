17f8553933caa8867b120a0269d35480c8b269e0
Michel Launier <michel.launier@icloud.com>
2015-05-04 08:38:58 -0400

    Move more iCanScript code inside iCanScript namespaces.

--- Product/iCanScript/Assets/iCanScript/Editor/LibraryDatabase/Variables/iCS_FunctionReturn.cs
+++ Product/iCanScript/Assets/iCanScript/Editor/LibraryDatabase/Variables/iCS_FunctionReturn.cs
@@ -3,7 +3,11 @@ using System;
 using System.Collections;
 using iCanScript;
 
-public class iCS_FunctionReturn : iCS_Parameter {
-    public iCS_FunctionReturn(string _name, Type _type)
-    : base(_name, _type, iCS_Types.DefaultValue(_type), iCS_ParamDirection.Out) {}
+namespace iCanScript.Editor {
+    
+    public class iCS_FunctionReturn : iCS_Parameter {
+        public iCS_FunctionReturn(string _name, Type _type)
+        : base(_name, _type, iCS_Types.DefaultValue(_type), iCS_ParamDirection.Out) {}
+    }
+
 }
--- Product/iCanScript/Assets/iCanScript/Editor/Utilities/iCS_TextUtility.cs
+++ Product/iCanScript/Assets/iCanScript/Editor/Utilities/iCS_TextUtility.cs
@@ -6,47 +6,52 @@ using System.Text;
 using System.Security;
 using System.Security.Cryptography;
 
-public static class iCS_TextUtility {
-    // ----------------------------------------------------------------------
-    public static string CalculateMD5Hash(string input)
-    {
-        // step 1, calculate MD5 hash from input
-        MD5 md5 = MD5.Create();
-        byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
-        byte[] hash = md5.ComputeHash(inputBytes);
-
-        // step 2, convert byte array to hex string
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < hash.Length; i++)
+namespace iCanScript {
+    
+    public static class iCS_TextUtility {
+        // ----------------------------------------------------------------------
+        public static string CalculateMD5Hash(string input)
         {
-            sb.Append(hash[i].ToString("X2"));
-        }
-        return sb.ToString();
-    }
+            // step 1, calculate MD5 hash from input
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
 
-    // ---------------------------------------------------------------------------------
-    // Converts the given string to a C# class name.
-    public static string ToCSharpName(string proposedName) {
-        StringBuilder fileName= new StringBuilder();
-        foreach(var c in proposedName) {
-            if(!Char.IsWhiteSpace(c)) {
-                fileName.Append((c == '_' || Char.IsLetterOrDigit(c)) ? c : '_');
+            // step 2, convert byte array to hex string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
             }
+            return sb.ToString();
         }
-        return fileName.ToString();
+
+        // ---------------------------------------------------------------------------------
+        // Converts the given string to a C# class name.
+        public static string ToCSharpName(string proposedName) {
+            StringBuilder fileName= new StringBuilder();
+            foreach(var c in proposedName) {
+                if(!Char.IsWhiteSpace(c)) {
+                    fileName.Append((c == '_' || Char.IsLetterOrDigit(c)) ? c : '_');
+                }
+            }
+            return fileName.ToString();
+        }
+
+        // ---------------------------------------------------------------------------------
+    	// Converts a string to it Unicode equivalent.
+    	public static string ToASCII(string inputString) {
+    		return Encoding.ASCII.GetString(Encoding.ASCII.GetBytes(inputString));
+    	}
+
+        // ---------------------------------------------------------------------------------
+        public static bool StartsWithAVowel(string str) {
+            switch(Char.ToUpper(str[0])) {
+                case 'A': case 'E': case 'I': case 'O': case 'U': return true;
+            }
+            return false;
+        }
+
     }
 
-    // ---------------------------------------------------------------------------------
-	// Converts a string to it Unicode equivalent.
-	public static string ToASCII(string inputString) {
-		return Encoding.ASCII.GetString(Encoding.ASCII.GetBytes(inputString));
-	}
-
-    // ---------------------------------------------------------------------------------
-    public static bool StartsWithAVowel(string str) {
-        switch(Char.ToUpper(str[0])) {
-            case 'A': case 'E': case 'I': case 'O': case 'U': return true;
-        }
-        return false;
-    }
 }
