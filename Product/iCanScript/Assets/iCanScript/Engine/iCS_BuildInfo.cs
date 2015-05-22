749679529ab7c4ffee5d77480982f1410986b751
Michel Launier <michel.launier@icloud.com>
2015-05-22 15:40:45 -0400

    Add release information.

--- Product/iCanScript/Assets/iCanScript/Engine/iCS_BuildInfo.cs
+++ Product/iCanScript/Assets/iCanScript/Engine/iCS_BuildInfo.cs
@@ -1,5 +1,5 @@
 namespace iCanScript.Internal.Engine {
   public static class iCS_BuildInfo {
-public const string kBuildDateStr= "15/05/20 17:09:33";
+public const string kBuildDateStr= "15/05/22 15:18:54";
   }
 }
