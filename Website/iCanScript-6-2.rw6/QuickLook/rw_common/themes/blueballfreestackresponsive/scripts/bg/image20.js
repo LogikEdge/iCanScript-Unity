51f47467ca65027b35add4f1275d94959c5b56a5
Michel Launier <michel.launier@icloud.com>
2015-06-11 16:36:20 -0400

    Remove obsoleted iCanScript website projects.

--- Website/iCanScript-6-2.rw6/QuickLook/rw_common/themes/blueballfreestackresponsive/scripts/bg/image20.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-	$('body, html').css('background-image', 'url(' + bg_20 + ')');
-	$("body, html").addClass("bgResource");
-});
--- Website/iCanScript-6-2.rw6/QuickLook/rw_common/themes/blueballfreestackresponsive/scripts/bg/image8.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-	$('body, html').css('background-image', 'url(' + bg_08 + ')');
-	$("body, html").addClass("bgResource");
-});
