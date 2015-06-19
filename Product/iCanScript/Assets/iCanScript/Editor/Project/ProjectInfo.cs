3d7e9b87b807341979600ca48cdcbda1aa19a529
Michel Launier <michel.launier@icloud.com>
2015-06-19 12:50:30 -0400

    Add Design folder to DevRoot

---
+++ Product/iCanScript/Assets/iCanScript/Editor/Project/ProjectInfo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace iCanScript.Internal.Editor {
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    /// This class contains all the project related information.
+    ///
+    /// The content is saved inside the project file.
+    ///
+    public class ProjectInfo {
+    }
+}
