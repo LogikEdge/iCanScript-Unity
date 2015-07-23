d10b303dc9b74f6358acd5b83d07b20bbfb7074c
Michel Launier <michel.launier@icloud.com>
2015-07-23 11:42:10 -0400

    Fix port editor not properly shown if port value is NULL.

--- Product/iCanScript/Assets/iCanScript/Engine/iCS_BuildInfo.cs
+++ Product/iCanScript/Assets/iCanScript/Engine/iCS_BuildInfo.cs
@@ -1,5 +1,5 @@
 namespace iCanScript.Internal.Engine {
   public static class iCS_BuildInfo {
-public const string kBuildDateStr= "15/07/23 10:41:08";
+public const string kBuildDateStr= "15/07/23 10:53:22";
   }
 }
