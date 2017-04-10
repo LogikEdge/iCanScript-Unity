df5e0e63aa7d750d887678b132e217187835f9e6
Reinual <michel.launier@icloud.com>
2017-04-09 22:59:35 -0400

    Remove another 5.5 deprecated scene management function.

--- Product/iCanScript/Assets/iCanScript/Editor/IStorage/iCS_IStorage_DataUpgrade.cs
+++ Product/iCanScript/Assets/iCanScript/Editor/IStorage/iCS_IStorage_DataUpgrade.cs
@@ -11,15 +11,16 @@ using iCanScript.TimeUtility;
 using iCanScript.MathUtility;
 using iCanScript.Logic;
 using CSharp.Primitives;
+using UnityEditor.SceneManagement;
 
 namespace iCanScript.Internal.Editor {
-    
+
     public partial class iCS_IStorage {
         // ----------------------------------------------------------------------
         /// Performs engine data upgarde before generating the editor data.
     	void PerformEngineDataUpgrade() {
             bool isUpgraded= false;
-        
+
             // PRE-PROCESSING ====================================================
             // Use this are to perform pre-processing data conversion.
     		Version softwareVersion= Version.Current;
@@ -27,7 +28,7 @@ namespace iCanScript.Internal.Editor {
     		// Extract the version of the storage.
     		Version storageVersion= new Version(EngineStorage.MajorVersion, EngineStorage.MinorVersion, EngineStorage.BugFixVersion);
     		if(softwareVersion.IsEqual(storageVersion)) { return; }
-		
+
             // -- Warn the user that an upgrade toke place --
             if(isUpgraded) {
     			ShowUpgradeDialog(softwareVersion);
@@ -37,7 +38,7 @@ namespace iCanScript.Internal.Editor {
         /// Performs editor data upgarde.
     	void PerformEditorDataUpgrade() {
             bool isUpgraded= false;
-        
+
             // PRE-PROCESSING ====================================================
             // Use this are to perform pre-processing data conversion.
     		Version softwareVersion= Version.Current;
@@ -45,7 +46,7 @@ namespace iCanScript.Internal.Editor {
     		// Extract the version of the storage.
     		Version storageVersion= new Version(EngineStorage.MajorVersion, EngineStorage.MinorVersion, EngineStorage.BugFixVersion);
     		if(softwareVersion.IsEqual(storageVersion)) { return; }
-		
+
             // -- Upgrade each version --
     		if(storageVersion.IsOlderThen(2,0,17)) {
                 isUpgraded |= V2_0_17_EditorUpgrade();
@@ -85,7 +86,7 @@ namespace iCanScript.Internal.Editor {
     	}
         // ----------------------------------------------------------------------
     	void SaveCurrentScene() {
-    		EditorApplication.SaveCurrentSceneIfUserWantsTo();
+    		EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
     	}
 
         // ======================================================================
@@ -123,7 +124,7 @@ namespace iCanScript.Internal.Editor {
                             isUpgraded= true;
                         }
                         else {
-                            Debug.LogWarning("Need to convert=> "+qualifiedTypeName+"."+methodName);                            
+                            Debug.LogWarning("Need to convert=> "+qualifiedTypeName+"."+methodName);
                         }
                     }
                 }
